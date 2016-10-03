48515e1d682909784a959f1d203b27dda49c77e0
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2016-10-03 13:19:08 +0100

    Scripts update

---
+++ post_setup.py
@@ -0,0 +1,50 @@
+import subprocess
+import pip
+import os
+from notebook.nbextensions import install_nbextension_python, enable_nbextension_python
+from notebook.serverextensions import toggle_serverextension_python
+import json
+
+
+# Install and enable the Geppetto Jupyter extension
+def run_nbextension_install(develop):
+    # Command: sudo jupyter nbextension install --py geppettoJupyter
+    print("Installing geppettoJupyter extension ...")
+    install_nbextension_python('geppettoJupyter', symlink=develop)
+
+    # Command: sudo jupyter nbextension enable --py geppettoJupyter
+    print("Enabling geppettoJupyter extensions ...")
+    isEnabled = enable_nbextension_python('geppettoJupyter')
+    if not isEnabled:
+        raise Exception('Problem enabling geppettoJupyter extension')
+
+    # Command: jupyter serverextension enable --py geppettoJupyter
+    print("Enabling server extensions ...")
+    toggle_serverextension_python('geppettoJupyter', enabled=True)
+
+def run():
+    print("Cloning Geppetto Jupyter (Python package)...")
+    subprocess.call(['git', 'clone', '--recursive', 'https://github.com/openworm/org.geppetto.frontend.jupyter.git'])
+
+    print("Checking out development branch for Geppetto Jupyter ...")
+    subprocess.call(['git', 'checkout', 'development'], cwd='org.geppetto.frontend.jupyter')
+    # We are checking out development so it's straightforward to commit and push changes
+    subprocess.call(['git', 'checkout', 'development'], cwd='org.geppetto.frontend.jupyter/src/geppettoJupyter/geppetto/')
+
+    print("Cloning Geppetto Neuron Configuration ...")
+    subprocess.call(['git', 'clone', 'https://github.com/MetaCell/geppetto-neuron.git'],
+                    cwd='org.geppetto.frontend.jupyter/src/geppettoJupyter/geppetto/src/main/webapp/extensions/')
+    print("Enabling Geppetto Neuron Configuration ...")
+    jsonFile = open(
+        'org.geppetto.frontend.jupyter/src/geppettoJupyter/geppetto/src/main/webapp/extensions/extensionsConfiguration.json',
+        "w+")
+    jsonFile.write(json.dumps({"geppetto-neuron/ComponentsInitialization": True}))
+    jsonFile.close()
+
+    print("Installing Geppetto Jupyter python package ...")
+    os.chdir('org.geppetto.frontend.jupyter')
+    pip.main(['install', '.', '--upgrade', '--no-deps', '--force-reinstall', '--install-option',
+              '--jupyter-notebook-path="http://localhost:8888/notebooks/libs/neuron-ui-demo.ipynb"'])
+
+    print("Installing Geppetto Jupyter Extension ...")
+    run_nbextension_install(False)
--- setup.py
+++ setup.py
@@ -1,61 +1,29 @@
 import setuptools
-import subprocess
-import pip
-import os
-from notebook.nbextensions import install_nbextension_python, enable_nbextension_python
-from notebook.serverextensions import toggle_serverextension_python
-import json
+from setuptools.command.install import install
 
-# Install and enable the Geppetto Jupyter extension 
-def run_nbextension_install(develop):
-    # Command: sudo jupyter nbextension install --py geppettoJupyter
-    print("Installing geppettoJupyter extension ...")
-    install_nbextension_python('geppettoJupyter', symlink=develop)
 
-    # Command: sudo jupyter nbextension enable --py geppettoJupyter
-    print("Enabling geppettoJupyter extensions ...")
-    isEnabled = enable_nbextension_python('geppettoJupyter')
-    if not isEnabled:
-        raise Exception('Problem enabling geppettoJupyter extension')
-    
-    # Command: jupyter serverextension enable --py geppettoJupyter
-    print("Enabling server extensions ...")
-    toggle_serverextension_python('geppettoJupyter', enabled=True)
-    
-    
-print("Cloning Geppetto Jupyter (Python package)...")
-subprocess.call(['git', 'clone', '--recursive', 'https://github.com/openworm/org.geppetto.frontend.jupyter.git'])
+class CustomInstallCommand(install):
 
-print("Checking out development branch for Geppetto Jupyter ...")
-subprocess.call(['git', 'checkout', 'development'], cwd = 'org.geppetto.frontend.jupyter')
-#We are checking out development so it's straightforward to commit and push changes
-subprocess.call(['git', 'checkout', 'development'], cwd = 'org.geppetto.frontend.jupyter/src/geppettoJupyter/geppetto/')
+    def run(self):
+        print("Installing NEURON_UI")
+        raise AttributeError("POBA")
+        install.run(self)
+        import post_setup
+        post_setup.run()
 
-print("Cloning Geppetto Neuron Configuration ...")
-subprocess.call(['git', 'clone', 'https://github.com/MetaCell/geppetto-neuron.git'], cwd = 'org.geppetto.frontend.jupyter/src/geppettoJupyter/geppetto/src/main/webapp/extensions/')
-print("Enabling Geppetto Neuron Configuration ...")
-jsonFile = open('org.geppetto.frontend.jupyter/src/geppettoJupyter/geppetto/src/main/webapp/extensions/extensionsConfiguration.json', "w+")
-jsonFile.write(json.dumps({"geppetto-neuron/ComponentsInitialization": True}))
-jsonFile.close()
-
-print("Installing Geppetto Jupyter python package ...")
-os.chdir('org.geppetto.frontend.jupyter')
-pip.main(['install', '.', '--upgrade', '--no-deps', '--force-reinstall', '--install-option','--jupyter-notebook-path="http://localhost:8888/notebooks/libs/neuron-ui-demo.ipynb"' ])
-
-print("Installing Geppetto Jupyter Extension ...")
-run_nbextension_install(False)
-
+        print("NEURON_UI installed successfully")
 
 
 setuptools.setup(
     name="neuron-ui",
     version="0.0.1",
+    cmdclass={
+        'install': CustomInstallCommand,
+    },
     url="https://github.com/MetaCell/NEURON-UI",
     author="MetaCell",
     description="Experimental UI for NEURON",
     long_description=open('README.md').read(),
-    packages=setuptools.find_packages('src'),
-    package_dir={'': 'src'},
     include_package_data=True,
     install_requires = [
         'ipywidgets>=5.1.5',
