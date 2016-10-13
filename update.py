827e7d59734d4e189e8ef0feb9b70a3f5360fd07
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2016-10-13 17:19:37 +0100

    Update script

--- update.py
+++ update.py
@@ -6,4 +6,6 @@ subprocess.call(['git', 'checkout', 'development'], cwd='org.geppetto.frontend.j
 subprocess.call(['git', 'pull'], cwd='org.geppetto.frontend.jupyter')
 subprocess.call(['git', 'checkout', 'development'], cwd='org.geppetto.frontend.jupyter/src/geppettoJupyter/geppetto/')
 subprocess.call(['git', 'pull'], cwd='org.geppetto.frontend.jupyter/src/geppettoJupyter/geppetto/')
+subprocess.call(['git', 'checkout', 'master'], cwd='org.geppetto.frontend.jupyter/src/geppettoJupyter/geppetto/src/main/webapp/extensions/geppetto-neuron/')
+subprocess.call(['git', 'pull'], cwd='org.geppetto.frontend.jupyter/src/geppettoJupyter/geppetto/src/main/webapp/extensions/geppetto-neuron/')
 subprocess.call(['python','install.py','overwrite'])
