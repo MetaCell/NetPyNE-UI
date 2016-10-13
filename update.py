f1f7529de9f6b87e5b7518a4129d46f18fa3bb51
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2016-10-13 17:08:50 +0100

    Update script checking out development

--- update.py
+++ update.py
@@ -2,6 +2,8 @@ import subprocess
 import os
 
 subprocess.call(['git', 'pull'])
+subprocess.call(['git', 'checkout', 'development'], cwd='org.geppetto.frontend.jupyter')
 subprocess.call(['git', 'pull'], cwd='org.geppetto.frontend.jupyter')
+subprocess.call(['git', 'checkout', 'development'], cwd='org.geppetto.frontend.jupyter/src/geppettoJupyter/geppetto/')
 subprocess.call(['git', 'pull'], cwd='org.geppetto.frontend.jupyter/src/geppettoJupyter/geppetto/')
 subprocess.call(['python','install.py','overwrite'])
