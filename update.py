ad8d672765f9b27c547d1d120af1e22c4b27aa80
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2016-11-22 12:59:26 +0000

    Scripts update

--- update.py
+++ update.py
@@ -1,10 +1,10 @@
 import subprocess
 
 subprocess.call(['git', 'pull'])
-subprocess.call(['git', 'checkout', 'integratingPlotWidget'], cwd='org.geppetto.frontend.jupyter')
+subprocess.call(['git', 'checkout', 'development'], cwd='org.geppetto.frontend.jupyter')
 subprocess.call(['git', 'pull'], cwd='org.geppetto.frontend.jupyter')
 subprocess.call(['git', 'checkout', 'integratingPlot'], cwd='org.geppetto.frontend.jupyter/src/geppettoJupyter/geppetto/')
 subprocess.call(['git', 'pull'], cwd='org.geppetto.frontend.jupyter/src/geppettoJupyter/geppetto/')
-subprocess.call(['git', 'checkout', 'integratingPlot'], cwd='org.geppetto.frontend.jupyter/src/geppettoJupyter/geppetto/src/main/webapp/extensions/geppetto-neuron/')
+subprocess.call(['git', 'checkout', 'master'], cwd='org.geppetto.frontend.jupyter/src/geppettoJupyter/geppetto/src/main/webapp/extensions/geppetto-neuron/')
 subprocess.call(['git', 'pull'], cwd='org.geppetto.frontend.jupyter/src/geppettoJupyter/geppetto/src/main/webapp/extensions/geppetto-neuron/')
 subprocess.call(['python','install.py','overwrite'])
