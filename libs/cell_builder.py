b1ed8baa53459f8e30522a75df6f8ef850d340bc
Adrian Quintana <adrianquintana@gmail.com>
2017-01-19 18:05:01 +0000

    used calculated section distance to extract segment. Add segment diameter

--- libs/cell_builder.py
+++ libs/cell_builder.py
@@ -15,39 +15,47 @@ class CellBuilder:
 
         self.geometry = None
         self.segment = None
-        radius_panel = neuron_utils.add_text_field_with_label('Radius', None)
-        length_panel = neuron_utils.add_text_field_with_label('Length', None)
+        section_diam_panel = neuron_utils.add_text_field_with_label('Section Diam', None)
+        section_length_panel = neuron_utils.add_text_field_with_label('Section Length', None)
+
+        segment_diam_panel = neuron_utils.add_text_field_with_label(
+            'Segment Diam', None)
 
         # Get textfields from panels
-        self.radius = radius_panel.items[1]
-        self.length = length_panel.items[1]
+        self.section_diam = section_diam_panel.items[1]
+        self.section_length = section_length_panel.items[1]
+        self.segment_diam = segment_diam_panel.items[1]
         self.save_button = neuron_utils.add_button('Save', self.modify_segment)
 
         self.cellBuilderPanel = neuron_utils.add_panel('Cell Builder', items=[
-            radius_panel, length_panel, self.save_button], widget_id='cellBuilderPanel', positionX=600, positionY=10)
+            segment_diam_panel, section_diam_panel, section_length_panel, self.save_button], widget_id='cellBuilderPanel', positionX=600, positionY=10)
         self.cellBuilderPanel.registerToEvent(
             [GeppettoJupyterModelSync.events_controller._events['Select']], self.updateValues)
         self.cellBuilderPanel.display()
 
-    def updateValues(self, dataId, groupNameIdentifier):
+    def updateValues(self, dataId, geometry_identifier, point):
         try:
             logging.debug('Updating values for Cell Builder')
 
             for geometry in GeppettoJupyterModelSync.current_model.geometries_raw:
-                if geometry.id == groupNameIdentifier:
+                if geometry.id == geometry_identifier:
                     logging.debug('Loading values for geometry ' +
-                                  str(groupNameIdentifier))
-
-                    # Update values
-                    self.radius.sync_value = str(
-                        geometry.python_variable["section"].diam)
-                    self.length.sync_value = str(
-                        geometry.python_variable["section"].L)
+                                  str(geometry_identifier))
 
                     # Update segment and geometry
+                    distance_to_selection_normalised = neuron_utils.calculate_normalised_distance_to_selection(
+                        geometry, point)
                     self.segment = geometry.python_variable[
-                        "section"](geometry.python_variable["segment"])
+                        "section"](distance_to_selection_normalised)
                     self.geometry = geometry
+
+                    # Update values
+                    self.segment_diam.sync_value =  str(
+                        self.segment.diam)
+                    self.section_diam.sync_value = str(
+                        geometry.python_variable["section"].diam)
+                    self.section_length.sync_value = str(
+                        geometry.python_variable["section"].L)
 
                     return True
             return False
@@ -59,9 +67,10 @@ class CellBuilder:
     def modify_segment(self, triggeredComponent, args):
         logging.debug('Modifying segment')
         self.geometry.python_variable[
-            "section"].diam = float(self.radius.sync_value)
+            "section"].diam = float(self.section_diam.sync_value)
         self.geometry.python_variable[
-            "section"].L = float(self.length.sync_value)
+            "section"].L = float(self.section_length.sync_value)
+        self.segment = float(self.segment_diam.sync_value)
 
         # Recreating Scene
         neuron_utils.extractGeometries()
