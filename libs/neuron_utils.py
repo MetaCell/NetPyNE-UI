d7432cce9ca91a468564c16e173027491ec0cc7f
Adrian Quintana <adrianquintana@gmail.com>
2017-01-24 20:40:55 +0000

    clean and refactor point process, add missing parameter

--- libs/neuron_utils.py
+++ libs/neuron_utils.py
@@ -9,6 +9,7 @@ from geppettoJupyter.geppetto_comm import GeppettoJupyterGUISync
 from neuron import h
 h.load_file("stdrun.hoc")
 
+
 def configure_logging():
     logger = logging.getLogger()
     fhandler = logging.FileHandler(filename='neuron.log', mode='a')
@@ -19,7 +20,43 @@ def configure_logging():
     logger.setLevel(logging.DEBUG)
     logging.debug('Log configured')
 
-def calculate_normalised_distance_to_selection(geometry, point):
+
+def calculate_segment_location(nseg, distance_to_selection_normalised, section_length):
+    interval = 1.0 / nseg
+    seg_index = int(distance_to_selection_normalised * nseg)
+    seg_loc_normalised = seg_index * interval + interval / 2
+    seg_loc = seg_loc_normalised * section_length
+    return seg_loc
+
+
+def calculate_distance_to_cylinder_location(geometry, seg_loc):
+    distance_to_seg_loc = 0
+    proximal = []
+    distal = []
+    for point_index in range(len(geometry.python_variable["section_points"]) - 1):
+        proximal = geometry.python_variable["section_points"][point_index]
+        distal = geometry.python_variable["section_points"][point_index + 1]
+        geometry_length = sqrt(pow(distal[0] - proximal[0], 2) + pow(
+            distal[1] - proximal[1], 2) + pow(distal[2] - proximal[2], 2))
+        if seg_loc < geometry_length + distance_to_seg_loc:
+            break
+        distance_to_seg_loc += geometry_length
+    return distal, proximal, distance_to_seg_loc
+
+
+def calculate_sphere_coordinates_and_radius(distal, proximal, seg_loc, distance_to_seg_loc):
+    average_radius = (proximal[3] + distal[3]) / 2
+    geometry_vector = [distal[0] - proximal[0],
+                       distal[1] - proximal[1], distal[2] - proximal[2]]
+    geometry_vector_length = sqrt(pow(
+        geometry_vector[0], 2) + pow(geometry_vector[1], 2) + pow(geometry_vector[2], 2))
+    distance_in_seg = (seg_loc - distance_to_seg_loc) / geometry_vector_length
+    sphere_coordinates = [proximal[0] + distance_in_seg * geometry_vector[0], proximal[1] +
+                          distance_in_seg * geometry_vector[1], proximal[2] + distance_in_seg * geometry_vector[2]]
+    return sphere_coordinates, average_radius
+
+
+def calculate_distance_to_selection(geometry, point):
     # Calculate segment
     geometry_index = int(geometry.id.rsplit('_', 1)[1])
     section_length = 0
@@ -28,23 +65,31 @@ def calculate_normalised_distance_to_selection(geometry, point):
         # Calculate geometry length
         proximal = geometry.python_variable["section_points"][point_index]
         distal = geometry.python_variable["section_points"][point_index + 1]
-        geometry_length = sqrt(pow(distal[0]-proximal[0],2) + pow(distal[1]-proximal[1],2) + pow(distal[2]-proximal[2],2))
+        geometry_length = sqrt(pow(distal[0] - proximal[0], 2) + pow(
+            distal[1] - proximal[1], 2) + pow(distal[2] - proximal[2], 2))
         section_length += geometry_length
         # Add geometry length or distance to selection
         if point_index < geometry_index:
             distance_to_selection += geometry_length
         elif point_index == geometry_index:
             # Calculate project point
-            geometry_vector = [distal[0]-proximal[0], distal[1]-proximal[1], distal[2]-proximal[2]]
-            point_vector = [point[0]-proximal[0], point[1]-proximal[1], point[2]-proximal[2]]
-            t_num = geometry_vector[0]*point_vector[0] + geometry_vector[1]*point_vector[1] + geometry_vector[2]*point_vector[2]
-            t_den = geometry_vector[0]*geometry_vector[0] + geometry_vector[1]*geometry_vector[1] + geometry_vector[2]*geometry_vector[2]
-            t = t_num/t_den
-            projected_point = [proximal[0] + t * geometry_vector[0], proximal[1] + t * geometry_vector[1], proximal[2] + t * geometry_vector[2]]
+            geometry_vector = [distal[0] - proximal[0],
+                               distal[1] - proximal[1], distal[2] - proximal[2]]
+            point_vector = [point[0] - proximal[0],
+                            point[1] - proximal[1], point[2] - proximal[2]]
+            t_num = geometry_vector[0] * point_vector[0] + geometry_vector[
+                1] * point_vector[1] + geometry_vector[2] * point_vector[2]
+            t_den = geometry_vector[0] * geometry_vector[0] + geometry_vector[
+                1] * geometry_vector[1] + geometry_vector[2] * geometry_vector[2]
+            t = t_num / t_den
+            projected_point = [proximal[0] + t * geometry_vector[0], proximal[
+                1] + t * geometry_vector[1], proximal[2] + t * geometry_vector[2]]
 
-            distance_to_selection += sqrt(pow(projected_point[0]-proximal[0],2) + pow(projected_point[1]-proximal[1],2) + pow(projected_point[2]-proximal[2],2))
+            distance_to_selection += sqrt(pow(projected_point[0] - proximal[0], 2) + pow(
+                projected_point[1] - proximal[1], 2) + pow(projected_point[2] - proximal[2], 2))
 
     return distance_to_selection, section_length
+
 
 def extractGeometries():
 
@@ -65,35 +110,41 @@ def extractGeometries():
         if 'pt3d' in sec['geom']:
             points = sec['geom']['pt3d']
             for i in range(len(points) - 1):
-                geometries.append(G.createGeometry(sec_name=sec_name, index=i, position=points[i], distal=points[i + 1], python_variable={'section': sec['neuronSec'], 'section_points': sec['geom']['pt3d']}))
+                geometries.append(G.createGeometry(sec_name=sec_name, index=i, position=points[i], distal=points[
+                                  i + 1], python_variable={'section': sec['neuronSec'], 'section_points': sec['geom']['pt3d']}))
 
     logging.debug("Sections and segments converted to Geppetto")
     logging.debug("Geometries found: " + str(len(geometries)))
     GeppettoJupyterModelSync.current_model.addGeometries(geometries)
     return geometries
 
-#GUI API
-def add_button(name, actions = None, value = None, extraData = None):
+# GUI API
+
+
+def add_button(name, actions=None, value=None, extraData=None):
     if value is not None:
         valueUnits = h.units(value)
         if valueUnits != '':
             name += " (" + valueUnits + ")"
 
-    button = GeppettoJupyterGUISync.ButtonSync(widget_id=G.newId(), widget_name=name, extraData = extraData)
+    button = GeppettoJupyterGUISync.ButtonSync(
+        widget_id=G.newId(), widget_name=name, extraData=extraData)
     if actions is not None:
         button.on_click(actions)
     return button
 
+
 def add_label(id, name):
-    return GeppettoJupyterGUISync.LabelSync(widget_id = G.newId(), widget_name = name, sync_value = id)
+    return GeppettoJupyterGUISync.LabelSync(widget_id=G.newId(), widget_name=name, sync_value=id)
+
 
 def add_text_field(name, value=None, read_only=False):
-    parameters = {'widget_id': G.newId(), 'widget_name' : name}
+    parameters = {'widget_id': G.newId(), 'widget_name': name}
     parameters['read_only'] = read_only
 
     if value is not None:
-        parameters['sync_value'] = str(eval("h."+ value))
-        parameters['extraData'] = {'originalValue': str(eval("h."+value))}
+        parameters['sync_value'] = str(eval("h." + value))
+        parameters['extraData'] = {'originalValue': str(eval("h." + value))}
         parameters['value'] = value
     else:
         parameters['value'] = ''
@@ -101,6 +152,7 @@ def add_text_field(name, value=None, read_only=False):
     if value is not None:
         textfield.on_blur(sync_value)
     return textfield
+
 
 def add_text_field_with_label(name, value=None, read_only=False):
     items = []
@@ -112,10 +164,12 @@ def add_text_field_with_label(name, value=None, read_only=False):
     panel.setDirection('row')
     return panel
 
-def add_drop_down(name, items):
-    return GeppettoJupyterGUISync.DropDownSync(widget_id = G.newId(), widget_name = name, items = items)
 
-def add_drop_down_with_label(name, drop_down_items = []):
+def add_drop_down(name, items):
+    return GeppettoJupyterGUISync.DropDownSync(widget_id=G.newId(), widget_name=name, items=items)
+
+
+def add_drop_down_with_label(name, drop_down_items=[]):
     items = []
     dropdown = add_drop_down(name, drop_down_items)
     items.append(add_label(dropdown.widget_id, name))
@@ -125,15 +179,18 @@ def add_drop_down_with_label(name, drop_down_items = []):
     panel.setDirection('row')
     return panel
 
+
 def add_text_field_and_button(name, value=None, create_checkbox=False, actions=None, extraData=None):
     items = []
     # Add Button
-    items.append(add_button(name, actions=None, value=value, extraData=extraData))
+    items.append(add_button(name, actions=None,
+                            value=value, extraData=extraData))
     # Create Text Field
     textField = add_text_field(name, value)
     if create_checkbox == True:
         # Create Checkbox
-        checkbox = add_checkbox("checkbox" + name, extraData={'targetComponent': textField})
+        checkbox = add_checkbox(
+            "checkbox" + name, extraData={'targetComponent': textField})
         checkbox.on_change(resetValueToOriginal)
         items.append(checkbox)
 
@@ -147,28 +204,35 @@ def add_text_field_and_button(name, value=None, create_checkbox=False, actions=N
     panel.setDirection('row')
     return panel
 
-def add_panel(name, items = [], widget_id=None, positionX=-1, positionY=-1):
-    if widget_id is None: widget_id = G.newId()
+
+def add_panel(name, items=[], widget_id=None, positionX=-1, positionY=-1):
+    if widget_id is None:
+        widget_id = G.newId()
     for item in items:
         item.embedded = True
-    return GeppettoJupyterGUISync.PanelSync(widget_id = widget_id, widget_name=name, items=items, embedded = False, positionX=positionX, positionY=positionY)
+    return GeppettoJupyterGUISync.PanelSync(widget_id=widget_id, widget_name=name, items=items, embedded=False, positionX=positionX, positionY=positionY)
 
-def add_checkbox(name, sync_value = 'false', extraData = None):
-    return GeppettoJupyterGUISync.CheckboxSync(widget_id=G.newId(), widget_name=name, sync_value = sync_value, extraData = extraData)
+
+def add_checkbox(name, sync_value='false', extraData=None):
+    return GeppettoJupyterGUISync.CheckboxSync(widget_id=G.newId(), widget_name=name, sync_value=sync_value, extraData=extraData)
+
 
 def resetValueToOriginal(triggeredComponent, args):
     logging.debug("Reseting value for textfield")
     logging.debug(triggeredComponent.extraData['targetComponent'].value)
-    logging.debug(triggeredComponent.extraData['targetComponent'].extraData['originalValue'])
+    logging.debug(triggeredComponent.extraData[
+                  'targetComponent'].extraData['originalValue'])
     triggeredComponent.sync_value = 'false'
     exec("h." + triggeredComponent.extraData['targetComponent'].value + "=" +
-            str(triggeredComponent.extraData['targetComponent'].extraData['originalValue']))
+         str(triggeredComponent.extraData['targetComponent'].extraData['originalValue']))
     #targetComponent.sync_value = str(targetComponent.extraData['originalValue'])
+
 
 def sync_value(triggeredComponent, args):
     logging.debug("Syncing value on blur for textfield")
     if triggeredComponent.value != None and triggeredComponent.value != '' and args['data'] != None:
         exec("h." + triggeredComponent.value + "=" + str(args['data']))
+
 
 def clickedCheckboxValue(triggeredComponent, args):
     logging.debug("Clicking checkbox due to change on textfield")
--- libs/point_process.py
+++ libs/point_process.py
@@ -5,7 +5,6 @@ Neuron Point Process
 import logging
 import neuron_utils
 from geppettoJupyter.geppetto_comm import GeppettoJupyterModelSync
-from math import sqrt, pow
 
 from neuron import h
 
@@ -18,131 +17,145 @@ class PointProcess:
         # self.point_processes = None
         self.current_point_process = None
 
-        drop_down_panel = neuron_utils.add_drop_down_with_label('Current Clamps', [{'id':'noSelection', 'value':'No segment selected'}])
+        drop_down_panel = neuron_utils.add_drop_down_with_label(
+            'Current Clamps', [{'id': 'noSelection', 'value': 'No segment selected'}])
         delay_panel = neuron_utils.add_text_field_with_label('Delay', None)
         duration_panel = neuron_utils.add_text_field_with_label(
             'Duration', None)
         amplitude_panel = neuron_utils.add_text_field_with_label(
             'Amplitude', None)
+        i_panel = neuron_utils.add_text_field_with_label(
+            'i', None)
         self.drop_down = drop_down_panel.items[1]
         self.drop_down.on_change(self.update_drop_down_selection)
         self.delay = delay_panel.items[1]
         self.duration = duration_panel.items[1]
         self.amplitude = amplitude_panel.items[1]
+        self.i = i_panel.items[1]
         self.save_button = neuron_utils.add_button(
             'Save', self.create_current_clamp)
 
         self.pointProcessPanel = neuron_utils.add_panel('Point Process', items=[
-            drop_down_panel, delay_panel, duration_panel, amplitude_panel, self.save_button], widget_id='pointProcessPanel', positionX=600, positionY=10)
+            drop_down_panel, delay_panel, duration_panel, amplitude_panel, i_panel, self.save_button], widget_id='pointProcessPanel', positionX=600, positionY=10)
         self.pointProcessPanel.register_to_event(
             [GeppettoJupyterModelSync.events_controller._events['Select']], self.updateValues)
         self.pointProcessPanel.display()
 
-    def update_drop_down_selection(self, triggeredComponent, args):
+    def update_drop_down_selection(self, triggered_component, args):
         logging.debug("Update drop drown")
-        logging.debug(args)
         if args['data'] == 'new':
-            self.current_point_process = None
-            self.delay.sync_value = str(0.0)
-            self.duration.sync_value = str(0.0)
-            self.amplitude.sync_value = str(0.0)
+            self.init_panel_for_new_point_process()
         else:
             for point_process in self.segment.point_processes():
-                logging.debug(point_process.hname())
                 if args['data'] == point_process.hname():
-                    self.current_point_process = point_process
-                    self.delay.sync_value = str(
-                        point_process.delay)
-                    self.duration.sync_value = str(
-                        point_process.dur)
-                    self.amplitude.sync_value = str(
-                        point_process.amp)
-
+                    self.init_panel_for_point_process(point_process)
 
     def updateValues(self, dataId, geometry_identifier, point):
 
         logging.debug('Updating values for Point Process')
-
         for geometry in GeppettoJupyterModelSync.current_model.geometries_raw:
             if geometry.id == geometry_identifier:
                 logging.debug('Loading values for geometry ' +
                               str(geometry_identifier))
 
-                distance_to_selection, section_length = neuron_utils.calculate_normalised_distance_to_selection(
+                # Calculate distance to selection
+                distance_to_selection, section_length = neuron_utils.calculate_distance_to_selection(
                     geometry, point)
-                distance_to_selection_normalised = distance_to_selection/section_length
+
+                # Normalise distance to selection
+                distance_to_selection_normalised = distance_to_selection / section_length
+                # Get Segment
                 self.segment = geometry.python_variable[
                     "section"](distance_to_selection_normalised)
 
-                point_processes = self.segment.point_processes()
-
+                # Get Point Processes for segment and init panel
                 self.drop_down.items = []
+                point_processes = self.segment.point_processes()
                 if len(point_processes) > 0:
-
                     for point_process in point_processes:
                         if point_process.hname().startswith('IClamp'):
-                            logging.debug("Point process found")
-                            self.current_point_process = point_process
-                            self.drop_down.add_child({'id':point_process.hname(), 'value':point_process.hname()})
-                            self.delay.sync_value = str(
-                                point_process.delay)
-                            self.duration.sync_value = str(
-                                point_process.dur)
-                            self.amplitude.sync_value = str(
-                                point_process.amp)
-                            self.drop_down.sync_value = point_process.hname()
+                            logging.debug(
+                                "Point process found with name " + point_process.hname())
+                            self.init_panel_for_point_process(point_process)
+                            self.drop_down.add_child(
+                                {'id': point_process.hname(), 'value': point_process.hname()})
 
                 else:
                     logging.debug("No point process found")
-                    self.delay.sync_value = str(0.0)
-                    self.duration.sync_value = str(0.0)
-                    self.amplitude.sync_value = str(0.0)
-                    self.drop_down.sync_value = 'new'
-                    self.current_point_process = None
+                    self.init_panel_for_new_point_process()
 
-                self.drop_down.add_child({'id':'new', 'value':'New Point Process'})
+                # Add new point process option
+                self.drop_down.add_child(
+                    {'id': 'new', 'value': 'New Point Process'})
 
-                # Calculate actual segment position
-                nseg = len(geometry.python_variable["section_points"]) - 1
-                interval = 1.0/nseg
-                segIndex = int(distance_to_selection_normalised*nseg)
-                seg_loc_normalised = segIndex * interval + interval/2
-                seg_loc = seg_loc_normalised * section_length
+                # Calculate actual segment position => segment.x
+                #nseg = len(geometry.python_variable["section_points"]) - 1
+                seg_loc = neuron_utils.calculate_segment_location(geometry.python_variable[
+                                                                  "section"].nseg, distance_to_selection_normalised, section_length)
 
-                distance_to_seg_loc = 0
-                proximal = []
-                distal = []
-                for point_index in range(len(geometry.python_variable["section_points"]) - 1):
-                    proximal = geometry.python_variable["section_points"][point_index]
-                    distal = geometry.python_variable["section_points"][point_index + 1]
-                    geometry_length = sqrt(pow(distal[0]-proximal[0],2) + pow(distal[1]-proximal[1],2) + pow(distal[2]-proximal[2],2))
-                    if seg_loc < geometry_length + distance_to_seg_loc:
-                        break
-                    distance_to_seg_loc += geometry_length
+                # Calculate distance to cylinder location origin and distal and
+                # proximal points
+                distal, proximal, distance_to_seg_loc = neuron_utils.calculate_distance_to_cylinder_location(
+                    geometry, seg_loc)
 
+                # Calculate radius for sphere depending on segment radiues
+                sphere_coordinates, average_radius = neuron_utils.calculate_sphere_coordinates_and_radius(
+                    distal, proximal, seg_loc, distance_to_seg_loc)
 
-                geometry_vector = [distal[0]-proximal[0], distal[1]-proximal[1], distal[2]-proximal[2]]
-                average_radius = (proximal[3] + distal[3])/2
-                geometry_vector_length = sqrt(pow(geometry_vector[0],2) + pow(geometry_vector[1],2) + pow(geometry_vector[2],2))
-                distance_in_seg = (seg_loc-distance_to_seg_loc)/geometry_vector_length
-                seg_loc_point = [proximal[0] + distance_in_seg * geometry_vector[0], proximal[1] + distance_in_seg * geometry_vector[1], proximal[2] + distance_in_seg * geometry_vector[2]]
-                
-                GeppettoJupyterModelSync.current_model.draw(seg_loc_point[0],seg_loc_point[1],seg_loc_point[2],average_radius*2)                
+                # Draw Sphere on middle segment point
+                GeppettoJupyterModelSync.current_model.draw(sphere_coordinates[0], sphere_coordinates[
+                                                            1], sphere_coordinates[2], average_radius * 2)
 
-    def create_current_clamp(self, triggeredComponent, args):
+    def create_current_clamp(self, triggered_component, args):
         logging.debug("Creating Current Clamp")
         if self.current_point_process is None:
+            # Init point process variable on current python model
             if not hasattr(GeppettoJupyterModelSync.current_python_model, 'point_processes'):
                 GeppettoJupyterModelSync.current_python_model.point_processes = []
-            pp = h.IClamp(self.segment)
-            self.current_point_process = pp
-            pp.delay = float(self.delay.sync_value)
-            pp.dur = float(self.duration.sync_value)
-            pp.amp = float(self.amplitude.sync_value)
-            GeppettoJupyterModelSync.current_python_model.point_processes.append(pp)
-            self.drop_down.add_child({'id':pp.hname(), 'value':pp.hname()})
-            self.drop_down.sync_value = pp.hname()
+
+            # Create current clamp
+            point_process = self.create_neuron_current_clamp()
+
+            # Refresh dropdown
+            self.drop_down.add_child({'id': point_process.hname(), 'value': point_process.hname()})
+            self.drop_down.sync_value =point_process.hname()
         else:
-            self.current_point_process.delay = float(self.delay.sync_value)
-            self.current_point_process.dur = float(self.duration.sync_value)
-            self.current_point_process.amp = float(self.amplitude.sync_value)
+            # Update current point process
+            self.update_neuron_current_clamp()
+
+    def update_neuron_current_clamp(self):
+        self.current_point_process.delay = float(self.delay.sync_value)
+        self.current_point_process.dur = float(self.duration.sync_value)
+        self.current_point_process.amp = float(self.amplitude.sync_value)
+        self.current_point_process.i = float(self.i.sync_value)
+
+    def create_neuron_current_clamp(self):
+        point_process = h.IClamp(self.segment)
+        point_process.delay = float(self.delay.sync_value)
+        point_process.dur = float(self.duration.sync_value)
+        point_process.amp = float(self.amplitude.sync_value)
+        point_process.i = float(self.i.sync_value)
+        GeppettoJupyterModelSync.current_python_model.point_processes.append(
+            point_process)
+        self.current_point_process = point_process
+        return point_process
+
+    def init_panel_for_new_point_process(self):
+        self.current_point_process = None
+        self.delay.sync_value = str(0.0)
+        self.duration.sync_value = str(0.0)
+        self.amplitude.sync_value = str(0.0)
+        self.i.sync_value = str(0.0)
+        self.drop_down.sync_value = 'new'
+
+    def init_panel_for_point_process(self, point_process):
+        self.current_point_process = point_process
+        self.delay.sync_value = str(
+            point_process.delay)
+        self.duration.sync_value = str(
+            point_process.dur)
+        self.amplitude.sync_value = str(
+            point_process.amp)
+        self.i.sync_value = str(
+            point_process.i)
+        self.drop_down.sync_value = point_process.hname()
