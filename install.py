9d18f4ff4b275551d00b0860c15eaef871dcfed9
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2017-01-25 12:52:46 +0000

    Update install.py

--- install.py
+++ install.py
@@ -8,16 +8,16 @@ print("Cloning Geppetto Jupyter (Python package)...")
 subprocess.call(['git', 'clone', '--recursive', 'https://github.com/openworm/org.geppetto.frontend.jupyter.git'])
 
 #print("Checking out development branch for Geppetto Jupyter ...")
-#subprocess.call(['git', 'checkout', 'master'], cwd='org.geppetto.frontend.jupyter')
+subprocess.call(['git', 'checkout', 'development'], cwd='org.geppetto.frontend.jupyter')
 
 #FIXME: Change to development once merged
-subprocess.call(['git', 'checkout', 'integratingPlot'], cwd='org.geppetto.frontend.jupyter/src/geppettoJupyter/geppetto/')
+subprocess.call(['git', 'checkout', 'development-jupyter'], cwd='org.geppetto.frontend.jupyter/src/geppettoJupyter/geppetto/')
 
 print("Cloning Geppetto Neuron Configuration ...")
 subprocess.call(['git', 'clone', 'https://github.com/MetaCell/geppetto-neuron.git'],
                 cwd='org.geppetto.frontend.jupyter/src/geppettoJupyter/geppetto/src/main/webapp/extensions/')
-#subprocess.call(['git', 'checkout', 'master'],
-#                cwd='org.geppetto.frontend.jupyter/src/geppettoJupyter/geppetto/src/main/webapp/extensions/geppetto-neuron/')
+subprocess.call(['git', 'checkout', 'development'],
+                cwd='org.geppetto.frontend.jupyter/src/geppettoJupyter/geppetto/src/main/webapp/extensions/geppetto-neuron/')
 
 enable_geppetto_neuron_extension()
 install_geppetto_jupyter_python((len(sys.argv) > 1 and sys.argv[1] == 'overwrite'))
