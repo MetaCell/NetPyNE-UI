a3b8e721cbbeb99e25ab71ad9f5d3f700df6db6f
Adrian Quintana <adrianquintana@gmail.com>
2017-01-30 20:49:03 +0000

    add derived state variables and plot

--- libs/space_plot.py
+++ libs/space_plot.py
@@ -6,6 +6,7 @@ import logging
 from geppettoJupyter.geppetto_comm import GeppettoCoreAPI as G
 from geppettoJupyter.geppetto_comm import GeppettoJupyterModelSync
 from neuron import h
+import time
 
 
 class SpacePlot:
@@ -17,9 +18,14 @@ class SpacePlot:
         self.section = None
         self.state_variables = []
 
-        self.plot_widget = G.plotVariable('Plot', position_x=90, position_y=405)
-        self.plot_widget.register_to_event(
-            [GeppettoJupyterModelSync.events_controller._events['Select']], self.refresh_data)
+        self.plot_widget = G.plotVariable(
+            'Plot', position_x=90, position_y=405)
+        # self.plot_widget.register_to_event(
+        #     [GeppettoJupyterModelSync.events_controller._events['Select']], self.refresh_data)
+        self.plot_widget_2 = G.plotVariable(
+            'Plot', position_x=490, position_y=405)
+        self.plot_widget_2.register_to_event(
+             [GeppettoJupyterModelSync.events_controller._events['Select']], self.refresh_data)
 
     def refresh_data(self, data, geometry_identifier, point):
         logging.debug('Refreshing space plot with selected geometry: ' +
@@ -46,8 +52,31 @@ class SpacePlot:
                     # self.vectors.append(vector)
                     self.state_variables.append(state_variable)
 
+                derived_state_variable = G.createDerivedStateVariable(id="space_plot", name="Space Plot",
+                                                                      units='mV', inputs=self.state_variables, normalizationFunction='SPACEPLOT')
+
+                derived_state_variable_2 = G.createDerivedStateVariable(id="space_plot_2", name="Space Plot 2",
+                                                                        units='nm', timeSeries=list(range(len(self.state_variables))), normalizationFunction='CONSTANT')
+
+                # FIXME: Quick and dirty hack to wait for variables to be
+                # property synced js side
+                time.sleep(1)
                 GeppettoJupyterModelSync.current_model.sync()
+
+                # FIXME: We can not be sured the new variables are created by
+                # this time probably is better if we register an event for
+                # model loaded and we listen to it
                 for state_variable in self.state_variables:
                     self.plot_widget.add_data(
-                        GeppettoJupyterModelSync.current_model.id + "." + state_variable.id)
+                        GeppettoJupyterModelSync.current_model.id + "." +
+                        state_variable.id)
+
+                self.plot_widget.plot_data()
+
+                self.plot_widget_2.add_data(
+                    GeppettoJupyterModelSync.current_model.id + "." + derived_state_variable.id)
+                self.plot_widget_2.add_data(
+                    GeppettoJupyterModelSync.current_model.id + "." + derived_state_variable_2.id)
+                self.plot_widget_2.plot_XY_data()
+
                 break
