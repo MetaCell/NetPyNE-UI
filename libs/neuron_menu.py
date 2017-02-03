b8f9ddc21949cebc71dfb83d5344c2f9f7a28576
Adrian Quintana <adrianquintana@gmail.com>
2017-02-03 14:24:08 +0000

    restructure class: remove dependency for neuron controller, instance new panel properly

--- libs/neuron_menu.py
+++ libs/neuron_menu.py
@@ -2,33 +2,55 @@ import logging
 import importlib
 import neuron_utils
 from geppettoJupyter.geppetto_comm import GeppettoJupyterModelSync
+from singleton import Singleton
 
+@Singleton
 class NeuronMenu:
     def __init__(self):
         logging.debug('Initializing Neuron Menu')
         self.items = []
-        self.items.append(neuron_utils.add_button('Run Control', self.loadModule, extraData = {'module': 'run_control', 'model':'RunControl'}))
-        self.items.append(neuron_utils.add_button('Point Process', self.loadModule, extraData = {'module': 'point_process', 'model':'PointProcess'}))
-        self.items.append(neuron_utils.add_button('Analysis', self.run_analysis))
-        self.items.append(neuron_utils.add_button('Cell Builder', self.loadModule, extraData = {'module': 'cell_builder', 'model':'CellBuilder'}))
-        self.items.append(neuron_utils.add_button('Space Plot', self.loadModule, extraData = {'module': 'space_plot', 'model':'SpacePlot'}))
+        self.items.append(neuron_utils.add_button('Run Control', self.show_run_control))
+        self.items.append(neuron_utils.add_button('Point Process', self.show_point_process))
+        # self.items.append(neuron_utils.add_button('Analysis', self.run_analysis))
+        self.items.append(neuron_utils.add_button('Cell Builder', self.show_cell_builder))
+        self.items.append(neuron_utils.add_button('Space Plot', self.show_space_plot))
 
         self.neuronMenuPanel = neuron_utils.add_panel('NEURON', items = self.items, widget_id = 'neuronMenuPanel', position_x =108, position_y=9, width = 570, height = 80)
         self.neuronMenuPanel.setDirection('row')
-        self.neuronMenuPanel.display() 
+        self.neuronMenuPanel.display()
 
-    def loadModule(self, triggeredComponent, args):
-        try:
-            logging.debug('Loading panel ' + triggeredComponent.extraData['module'])
-            #FIXME: Check if it works in python 2
-            module = importlib.import_module(triggeredComponent.extraData['module'])
-            getattr(module, triggeredComponent.extraData['model'])()
+    def shake_panel(self):
+        self.neuronMenuPanel.shake()
 
-        except Exception as e:
-            logging.exception("Unexpected error initialising panel")
-            raise
+    def show_point_process(self, triggeredComponent, args):
+        from point_process import PointProcess
+        return PointProcess.Instance()
 
-    def run_analysis(self, triggeredComponent, args):
-        logging.debug('Running analysis for current model')
-        if GeppettoJupyterModelSync.current_python_model:
-            GeppettoJupyterModelSync.current_python_model.analysis()
+    def show_space_plot(self, triggeredComponent, args):
+        from space_plot import SpacePlot
+        return SpacePlot.Instance()
+
+    def show_cell_builder(self, triggeredComponent, args):
+        from cell_builder import CellBuilder
+        return CellBuilder.Instance()
+
+    def show_run_control(self, triggeredComponent, args):
+        from run_control import RunControl
+        return RunControl.Instance()
+
+
+
+    # def loadModule(self, triggeredComponent, args):
+    #     try:
+    #         logging.debug('Loading panel ' + triggeredComponent.extraData['module'])
+    #         module = importlib.import_module(triggeredComponent.extraData['module'])
+    #         getattr(module, triggeredComponent.extraData['model'])()
+
+    #     except Exception as e:
+    #         logging.exception("Unexpected error initialising panel")
+    #         raise
+
+    # def run_analysis(self, triggeredComponent, args):
+    #     logging.debug('Running analysis for current model')
+    #     if GeppettoJupyterModelSync.current_python_model:
+    #         GeppettoJupyterModelSync.current_python_model.analysis()
