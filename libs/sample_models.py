71a19a1b19ba933cfad11f18fe2f4cbebc4f5087
Adrian Quintana <adrianquintana@gmail.com>
2017-02-12 21:52:28 +0000

    restart the kernel (from js) before loading a new model

--- libs/sample_models.py
+++ libs/sample_models.py
@@ -4,21 +4,27 @@ import neuron_utils
 from geppettoJupyter.geppetto_comm import GeppettoJupyterModelSync
 from singleton import Singleton
 
-from neuron import h
 
 @Singleton
 class SampleModels:
+
     def __init__(self):
         logging.debug('Initializing Samples panel')
         self.items = []
-        self.items.append(neuron_utils.add_button('Single compartment Hodgkin-Huxley', self.loadModule, extraData = {'module': 'verysimple_cell', 'model':'VerySimpleCell'}))
-        self.items.append(neuron_utils.add_button('Multi compartments Hodgkin-Huxley', self.loadModule, extraData = {'module': 'simple_cell', 'model':'SimpleCell'}))
+        self.items.append(neuron_utils.add_button('Single compartment Hodgkin-Huxley',
+                                                  self.loadModule, extraData={'module': 'verysimple_cell', 'model': 'VerySimpleCell'}))
+        self.items.append(neuron_utils.add_button('Multi compartments Hodgkin-Huxley',
+                                                  self.loadModule, extraData={'module': 'simple_cell', 'model': 'SimpleCell'}))
         #self.items.append(neuron_utils.add_button('Simple network', self.loadModule, extraData = {'module': 'simple_network', 'model':'SimpleNetwork'}))
-        self.items.append(neuron_utils.add_button('Ring network', self.loadModule, extraData = {'module': 'ring', 'model':'Ring'}))
-        self.items.append(neuron_utils.add_button('CA3 Pyramidal', self.loadModule, extraData = {'module': 'CA3_pyramidal', 'model':'CA3_pyramidal'}))
-        self.items.append(neuron_utils.add_button('PT Cell', self.loadModule, extraData = {'module': 'PTcell', 'model':'PTcell'}))
+        self.items.append(neuron_utils.add_button(
+            'Ring network', self.loadModule, extraData={'module': 'ring', 'model': 'Ring'}))
+        self.items.append(neuron_utils.add_button('CA3 Pyramidal', self.loadModule, extraData={
+                          'module': 'CA3_pyramidal', 'model': 'CA3_pyramidal'}))
+        self.items.append(neuron_utils.add_button(
+            'PT Cell', self.loadModule, extraData={'module': 'PTcell', 'model': 'PTcell'}))
 
-        self.loadModelPanel = neuron_utils.add_panel('Sample NEURON Models', items = self.items, widget_id = 'loadModelPanel', position_x =108, position_y=125, width = 287)
+        self.loadModelPanel = neuron_utils.add_panel(
+            'Sample NEURON Models', items=self.items, widget_id='loadModelPanel', position_x=108, position_y=125, width=287)
         self.loadModelPanel.on_close(self.close)
         self.loadModelPanel.display()
 
@@ -36,13 +42,25 @@ class SampleModels:
 
     def loadModule(self, triggeredComponent, args):
         try:
-            #FIXME: Not working when moving from CA3 Pyramidal to simple cell
-            h('forall delete_section()')
+            logging.debug(GeppettoJupyterModelSync.current_python_model)
 
-            logging.debug('Loading model ' + triggeredComponent.extraData['module'])
-            #FIXME: Check if it works in python 2
-            module = importlib.import_module("models." + triggeredComponent.extraData['module'])
-            GeppettoJupyterModelSync.current_python_model = getattr(module, triggeredComponent.extraData['model'])()
+            if (GeppettoJupyterModelSync.current_python_model is None):
+                logging.debug('Loading model ' +
+                              triggeredComponent.extraData['module'])
+                module = importlib.import_module(
+                    "models." + triggeredComponent.extraData['module'])
+                GeppettoJupyterModelSync.current_python_model = getattr(
+                    module, triggeredComponent.extraData['model'])()
+            else:
+                # If there is a model loaded -> Restart kernel server (from js)
+                # and load a new model
+                GeppettoJupyterModelSync.current_model.reload(
+                    triggeredComponent.extraData['module'], triggeredComponent.extraData['model'])
+
+            # Failed attempt to reload the model from python
+            # import IPython
+            # ipython = IPython.get_ipython()
+            # ipython.magic()
 
         except Exception as e:
             logging.exception("Unexpected error loading model")
