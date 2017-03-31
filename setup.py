9b978fb279861fc4a30c99759f1dbc3ec30d4000
Adrian Quintana <adrianquintana@gmail.com>
2017-03-31 14:22:12 +0100

    update information and add scripts parameter

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ setuptools.setup(
     name="neuron_ui",
     version="0.0.1",
     url="https://github.com/MetaCell/NEURON-UI",
-    author="The Geppetto Development Team",
-    author_email="info@geppetto.org",
+    author="MetaCell",
+    author_email="info@metacell.us",
     description="User interface for NEURON based on web technologies and Jupyter",
     license="MIT",
     long_description=open('README.rst').read(),
     packages=['neuron_ui'],
+    scripts=['NEURON_UI'],
     install_requires=[
         'jupyter_geppetto>=0.3.5'
     ],
