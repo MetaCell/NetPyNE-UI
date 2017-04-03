053eb1580da107beced0d7afc88a7198bc3d1f66
Adrian Quintana <adrianquintana@gmail.com>
2017-04-03 23:10:55 +0100

    iterate over packages and include package data

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(
+setup(
     name="neuron_ui",
-    version="0.0.1",
+    version="0.0.1.1",
     url="https://github.com/MetaCell/NEURON-UI",
     author="MetaCell",
     author_email="info@metacell.us",
     description="User interface for NEURON based on web technologies and Jupyter",
     license="MIT",
     long_description=open('README.rst').read(),
-    packages=['neuron_ui'],
+    packages=find_packages(),
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.mod', '*.inc', '*.inc', '*.hoc', '*.multi', '*.multi_new', '*.c', '*.lo', 'x86_64']
+        # And include any *.msg files found in the 'hello' package, too:
+        # 'hello': ['*.msg'],
+    },
     scripts=['NEURON-UI'],
     install_requires=[
         'jupyter_geppetto>=0.3.5'
