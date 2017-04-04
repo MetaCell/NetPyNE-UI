890711c0cb6773bcbcbe6c11445730a942f36382
Adrian Quintana <adrianquintana@gmail.com>
2017-04-05 00:12:14 +0100

    update branches and remove stash

--- utilities/install.py
+++ utilities/install.py
@@ -5,7 +5,7 @@ from utils import *
 
 
 print("Cloning Geppetto Jupyter (Python package)...")
-subprocess.call(['git', 'clone', '--recursive', '-b', 'pipImprovement', 'https://github.com/openworm/org.geppetto.frontend.jupyter.git'], cwd='../')
+subprocess.call(['git', 'clone', '--recursive', '-b', 'development', 'https://github.com/openworm/org.geppetto.frontend.jupyter.git'], cwd='../')
 
 subprocess.call(['git', 'checkout', 'tags/v0.3.4.jupyterStable3'], cwd='../org.geppetto.frontend.jupyter/src/jupyter_geppetto/geppetto/')
 
@@ -13,7 +13,7 @@ print("Cloning Geppetto Neuron Configuration ...")
 subprocess.call(['git', 'clone', 'https://github.com/MetaCell/geppetto-neuron.git'],
                 cwd='../org.geppetto.frontend.jupyter/src/jupyter_geppetto/geppetto/src/main/webapp/extensions/')
 
-subprocess.call(['git', 'checkout', 'installationImprovements'], cwd='../org.geppetto.frontend.jupyter/src/jupyter_geppetto/geppetto/src/main/webapp/extensions/geppetto-neuron/')
+subprocess.call(['git', 'checkout', 'development'], cwd='../org.geppetto.frontend.jupyter/src/jupyter_geppetto/geppetto/src/main/webapp/extensions/geppetto-neuron/')
 
 enable_geppetto_neuron_extension()
 
--- utilities/update.py
+++ utilities/update.py
@@ -7,17 +7,18 @@ except NameError: pass
 reply = input("Any uncommited change to your jupyter notebook will be stashed. Are you sure you want to update NEURON-UI? (y/n)")
 
 if reply[0] == 'y':
-    subprocess.call(['git', 'stash'])
     subprocess.call(['git', 'pull'])
-    subprocess.call(['git', 'checkout', 'master'], cwd='../org.geppetto.frontend.jupyter')
+    subprocess.call(['git', 'checkout', 'development'], cwd='../org.geppetto.frontend.jupyter')
     subprocess.call(['git', 'pull'], cwd='../org.geppetto.frontend.jupyter')
     subprocess.call(['git', 'checkout', 'tags/v0.3.4.jupyterStable3'], cwd='../org.geppetto.frontend.jupyter/src/jupyter_geppetto/geppetto/')
     subprocess.call(['git', 'pull'], cwd='../org.geppetto.frontend.jupyter/src/jupyter_geppetto/geppetto/')
-    subprocess.call(['git', 'checkout', 'master'], cwd='../org.geppetto.frontend.jupyter/src/jupyter_geppetto/geppetto/src/main/webapp/extensions/geppetto-neuron/')
+    subprocess.call(['git', 'checkout', 'development'], cwd='../org.geppetto.frontend.jupyter/src/jupyter_geppetto/geppetto/src/main/webapp/extensions/geppetto-neuron/')
     subprocess.call(['git', 'pull'], cwd='../org.geppetto.frontend.jupyter/src/jupyter_geppetto/geppetto/src/main/webapp/extensions/geppetto-neuron/')
     enable_geppetto_neuron_extension()
-    install_geppetto_jupyter_python(True)
+    install_package(True, '../org.geppetto.frontend.jupyter')
     run_nbextension_install(False)
+
+    install_package(True, '..')
 else:
     print("Exit without updating")
 
