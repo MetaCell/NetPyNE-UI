0367e2adc446dfdc87bcc34df4551163927f6a56
Adrian Quintana <adrianquintana@gmail.com>
2017-04-05 00:08:00 +0100

    add classifiers

--- setup.py
+++ setup.py
@@ -14,6 +14,18 @@ setup(
         '': ['*.hoc']
     },
     scripts=['NEURON-UI'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
+    ],
     install_requires=[
         'jupyter_geppetto>=0.3.5'
     ],
