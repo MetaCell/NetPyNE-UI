95bb881cf1dd1f16e510c05bb303120f537b9b6c
Adrian Quintana <adrianquintana@gmail.com>
2017-06-14 00:50:30 +0100

    install extension in dev way

--- utilities/install.py
+++ utilities/install.py
@@ -7,7 +7,7 @@ from utils import *
 print("Cloning Geppetto Jupyter (Python package)...")
 subprocess.call(['git', 'clone', '--recursive', '-b', 'geppetto036', 'https://github.com/openworm/org.geppetto.frontend.jupyter.git'], cwd='../')
 
-subprocess.call(['git', 'checkout', 'development'], cwd='../org.geppetto.frontend.jupyter/src/jupyter_geppetto/geppetto/')
+subprocess.call(['git', 'checkout', 'development-jupyter'], cwd='../org.geppetto.frontend.jupyter/src/jupyter_geppetto/geppetto/')
 
 print("Cloning Geppetto Neuron Configuration ...")
 subprocess.call(['git', 'clone', 'https://github.com/MetaCell/geppetto-neuron.git'],
@@ -17,10 +17,15 @@ subprocess.call(['git', 'checkout', 'geppetto036'], cwd='../org.geppetto.fronten
 
 enable_geppetto_neuron_extension()
 
+subprocess.call(['npm', 'install'], cwd='../org.geppetto.frontend.jupyter/src/jupyter_geppetto/geppetto/src/main/webapp/')
+subprocess.call(['npm', 'run', 'build-dev-noTest'], cwd='../org.geppetto.frontend.jupyter/src/jupyter_geppetto/geppetto/src/main/webapp/')
+
 print("Installing jupyter_geppetto python package ...")
-install_package((len(sys.argv) > 1 and sys.argv[1] == 'overwrite'), '../org.geppetto.frontend.jupyter')
+subprocess.call(['pip', 'install', '-e', '.'], cwd='../org.geppetto.frontend.jupyter')
 print("Installing jupyter_geppettor Extension ...")
-run_nbextension_install(False)
+subprocess.call(['jupyter', 'nbextension', 'install', '--py', '--symlink', 'jupyter_geppetto'], cwd='../org.geppetto.frontend.jupyter')
+subprocess.call(['jupyter', 'nbextension', 'enable', '--py', 'jupyter_geppetto'], cwd='../org.geppetto.frontend.jupyter')
 
 print("Installing neuron_ui python package ...")
-install_package((len(sys.argv) > 1 and sys.argv[1] == 'overwrite'), '..')
+subprocess.call(['pip', 'install', '-e', '.'], cwd='../org.geppetto.frontend.jupyter')
+
