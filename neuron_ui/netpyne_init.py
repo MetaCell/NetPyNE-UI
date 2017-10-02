bcd12e3db873cbd6c67830c0ae684e330283e1bb
Adrian Quintana <adrianquintana@gmail.com>
2017-10-02 08:34:48 +0100

    add metadata

--- neuron_ui/netpyne_init.py
+++ neuron_ui/netpyne_init.py
@@ -1,6 +1,7 @@
 import logging
 from netpyne import specs
 from netpyne.tests import tests
+from netpyne.metadata import metadata
 import json
 from neuron_ui import neuron_utils
 from jupyter_geppetto.geppetto_comm import GeppettoJupyterModelSync
@@ -10,6 +11,6 @@ simConfig = specs.SimConfig()
 
 neuron_utils.createProject(name='SampleProject')
 
-GeppettoJupyterModelSync.current_model.original_model = json.dumps({'netParams': netParams.__dict__, 'simConfig': simConfig.__dict__})
+GeppettoJupyterModelSync.current_model.original_model = json.dumps({'netParams': netParams.__dict__, 'simConfig': simConfig.__dict__, 'metadata': metadata.metadata})
 
 logging.debug(GeppettoJupyterModelSync.current_model.original_model)
