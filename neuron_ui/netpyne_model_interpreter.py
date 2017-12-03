71872addaefcbff756bc18d440693178519b909e
Adrian Quintana <adrianquintana@gmail.com>
2017-12-03 13:11:52 +0000

    clean up, improve performance

--- neuron_ui/netpyne_model_interpreter.py
+++ neuron_ui/netpyne_model_interpreter.py
@@ -23,12 +23,8 @@ class NetPyNEModelInterpreter():
             name='netpynelib')
         geppetto_model.libraries.append(netpyne_geppetto_library)
 
-
         self.extractPopulations(netpyne_model, netpyne_geppetto_library, geppetto_model)
-        # self.extractCellTypes(
-        #     netpyne_model, netpyne_geppetto_library, geppetto_model)
-        self.extractInstances(
-            netpyne_model, netpyne_geppetto_library, geppetto_model)
+        self.extractInstances(netpyne_model, netpyne_geppetto_library, geppetto_model)
 
         return geppetto_model
 
@@ -55,7 +51,7 @@ class NetPyNEModelInterpreter():
                 prevL = float(prevL + sec['geom']['L'])
                 sec['geom']['pt3d'].append(
                     [offset + 0, prevL, 0, sec['geom']['diam']])
-            elif secName in ['axon']:  # set 3d geom of axon
+            elif secName in ['axon', 'dend']:  # set 3d geom of axon
                 sec['geom']['pt3d'].append(
                     [offset + 0, 0, 0, sec['geom']['diam']])
                 sec['geom']['pt3d'].append(
@@ -64,6 +60,8 @@ class NetPyNEModelInterpreter():
                 sec['geom']['pt3d'].append(
                     [offset + 0, 0, 0, sec['geom']['diam']])
                 nseg = sec['geom']['nseg']
+                logging.debug('takasec')
+                logging.debug(sec)
                 if nseg != {}:
                     for i in range(nseg):
                         sec['geom']['pt3d'].append(
@@ -72,134 +70,58 @@ class NetPyNEModelInterpreter():
 
 
     def extractPopulations(self, netpyne_model, netpyne_geppetto_library, geppetto_model):
-        
         network = pygeppetto.CompositeType(id='network_netpyne', name='network_netpyne')
         netpyne_geppetto_library.types.append(network)
 
-        for key, value in netpyne_model.net.pops.items():
-            print key
-            print value.__dict__
-            print value.tags['cellType']
-
-            
-            
-            cellType = pygeppetto.CompositeType(id=value.tags['cellType'], name=value.tags['cellType'], abstract= False)
-            visualType = pygeppetto.CompositeVisualType(id='cellMorphology', name='cellMorphology')
-            cellType.visualType = visualType
-
-            print value.rand
-            print value.cellModelClass
-            print value.cellModelClass.__dict__
-
-            
-            elements = []
-            for index, cell in enumerate(sim.net.cells):
-                if cell.tags['pop'] == value.tags['pop']:
-
-                    if len(visualType.variables) == 0:
-                        secs = cell.secs
-                        if 'pt3d' not in list(secs.values())[0]['geom']:
-                            secs = self.convertTo3DGeoms(secs)
-                        for sec_name, sec in secs.items():
-
-                            if 'pt3d' in sec['geom']:
-                                points = sec['geom']['pt3d']
-                                for i in range(len(points) - 1):
-                                    if sec_name == 'soma':
-                                        visualType.variables.append(self.factory.createSphere(sec_name,
-                                                                                                radius=float(points[i][3] / 2),
-                                                                                                position=Point(x=float(points[i][0]),y=float(points[i][1]), z=float(points[i][2]))))
-                                    else:
-                                        visualType.variables.append(self.factory.createCylinder(sec_name,
-                                                                                                bottomRadius=float(points[i][3] / 2),
-                                                                                                topRadius=float(points[i + 1][3] / 2),
-                                                                                                position=Point(x=float(points[i][0]),y=float(points[i][1]), z=float(points[i][2])),
-                                                                                                distal=Point(x=float(points[i + 1][0]), y=float(points[i + 1][1]), z=float(points[i + 1][2]))))
-                    
-
-                    elements.append(ArrayElement(index=index , position=Point(x=0.0, y=0.0, z=0.0)))
-
-                    print sim.net.cells
-            
-            
-            netpyne_geppetto_library.types.append(cellType)
-            netpyne_geppetto_library.types.append(visualType)
+        populations = {}
+        for index, cell in enumerate(sim.net.cells):
+            if cell.tags['pop'] not in populations:
+                cellType = pygeppetto.CompositeType(id=cell.tags['cellType'], name=cell.tags['cellType'], abstract= False)
+                visualType = pygeppetto.CompositeVisualType(id='cellMorphology', name='cellMorphology')
+                cellType.visualType = visualType
 
 
-            defaultValue = ArrayValue(elements=elements)
-            arrayType = pygeppetto.ArrayType(size=value.tags['numCells'],
+                defaultValue = ArrayValue(elements=[])
+                arrayType = pygeppetto.ArrayType(size=0,
                                                     arrayType=cellType,
-                                                    id=key,
-                                                    name=key,
+                                                    id=cell.tags['pop'],
+                                                    name=cell.tags['pop'],
                                                     defaultValue= defaultValue)
-            netpyne_geppetto_library.types.append(arrayType)
 
+                arrayVariable = pygeppetto.Variable(id=cell.tags['pop'])
+                arrayVariable.types.append(arrayType)
+                network.variables.append(arrayVariable)
 
-            arrayVariable = pygeppetto.Variable(id=key)
-            arrayVariable.types.append(arrayType)
-            network.variables.append(arrayVariable)
+                netpyne_geppetto_library.types.append(cellType)
+                netpyne_geppetto_library.types.append(visualType)
+                netpyne_geppetto_library.types.append(arrayType)
 
-        
+                populations[cell.tags['pop']] = arrayType
 
-        
+                secs = cell.secs
+                if 'pt3d' not in list(secs.values())[0]['geom']:
+                    secs = self.convertTo3DGeoms(secs)
+                
+                for sec_name, sec in secs.items():
 
-    def extractCellTypes(self, netpyne_model, netpyne_geppetto_library, geppetto_model):
+                    if 'pt3d' in sec['geom']:
+                        points = sec['geom']['pt3d']
+                        for i in range(len(points) - 1):
+                            if sec_name == 'soma':
+                                visualType.variables.append(self.factory.createSphere(sec_name,
+                                                                                        radius=float(points[i][3] / 2),
+                                                                                        position=Point(x=float(points[i][0]),y=float(points[i][1]), z=float(points[i][2]))))
+                            else:
+                                visualType.variables.append(self.factory.createCylinder(sec_name,
+                                                                                        bottomRadius=float(points[i][3] / 2),
+                                                                                        topRadius=float(points[i + 1][3] / 2),
+                                                                                        position=Point(x=float(points[i][0]),y=float(points[i][1]), z=float(points[i][2])),
+                                                                                        distal=Point(x=float(points[i + 1][0]), y=float(points[i + 1][1]), z=float(points[i + 1][2]))))
+                
 
-        secDic, secLists, synMechs, globs = utils.getCellParams(
-            None, None, None)
-
-        logging.debug(secDic)
-        logging.debug(secLists)
-        logging.debug(synMechs)
-        logging.debug(globs)
-
-        secs = self.convertTo3DGeoms(secDic)
-
-        type = pygeppetto.CompositeType(id='cell', name='cell')
-        visualType = pygeppetto.CompositeVisualType(id='cellMorphology', name='cellMorphology')
-        type.visualType = visualType
-
-        logging.debug("Converting sections and segments to Geppetto")
-        for sec_name, sec in secs.items():
-            if 'pt3d' in sec['geom']:
-                points = sec['geom']['pt3d']
-                for i in range(len(points) - 1):
-                    if sec_name == 'soma':
-                        visualType.variables.append(self.factory.createSphere(sec_name,
-                                                                                radius=float(points[i][3] / 2),
-                                                                                position=Point(x=float(points[i][0]),y=float(points[i][1]), z=float(points[i][2]))))
-                    else:
-                        visualType.variables.append(self.factory.createCylinder(sec_name,
-                                                                                bottomRadius=float(points[i][3] / 2),
-                                                                                topRadius=float(points[i + 1][3] / 2),
-                                                                                position=Point(x=float(points[i][0]),y=float(points[i][1]), z=float(points[i][2])),
-                                                                                distal=Point(x=float(points[i + 1][0]), y=float(points[i + 1][1]), z=float(points[i + 1][2]))))
-
-
-        logging.debug("Sections and segments converted to Geppetto")
-
-        # type = pygeppetto.CompositeType(id='cell',name='cell')
-        # visualType = pygeppetto.CompositeVisualType(id='cellMorphology',name='cellMorphology')
-        # type.visualType=visualType
-        # visualType.variables.append(self.factory.createCylinder('apical0_1', bottomRadius=3.0, topRadius=3.0, position=Point(y=17.0), distal=Point(y=77.0)))
-        # visualType.variables.append(self.factory.createCylinder('apical1_5', bottomRadius=1.5, topRadius=1.5, position=Point(y=77.0), distal=Point(x=-150.0,y=77.0)))
-        # visualType.variables.append(self.factory.createCylinder('apical2_2', bottomRadius=2.2, topRadius=2.2, position=Point(y=77.0), distal=Point(y=477.0)))
-        # visualType.variables.append(self.factory.createCylinder('apical2_2', bottomRadius=1.45, topRadius=1.45, position=Point(y=477.0), distal=Point(y=877.0)))
-        # visualType.variables.append(self.factory.createCylinder('basal2_8', bottomRadius=2.5, topRadius=2.5, position=Point(y=-50.0), distal=Point(x=-106.07,y=-156.07)))
-        # visualType.variables.append(self.factory.createCylinder('basal1_7', bottomRadius=2.5, topRadius=2.5, position=Point(y=-50.0), distal=Point(x=-106.07,y=-156.07)))
-        # visualType.variables.append(self.factory.createCylinder('basal0_6', bottomRadius=2.0, topRadius=2.0, distal=Point(y=-50.0)))
-        # visualType.variables.append(self.factory.createCylinder('soma_0', bottomRadius=11.5, topRadius=11.5, distal=Point(y=17.0)))
-        # visualType.variables.append(self.factory.createCylinder('apical4_4', bottomRadius=11.5, topRadius=11.5, position=Point(y=877.0),distal=Point(y=1127.0)))
-
-        netpyne_geppetto_library.types.append(type)
-        netpyne_geppetto_library.types.append(visualType)
-
-        # print netpyne_model.net.allCells[0]
-        # sim.net.allCells[0]['secs']
-        # sim.net.allCells[0]['conns']
-        # sim.net.allCells[0]['conns']
-        pass
-
+            populations[cell.tags['pop']].size = populations[cell.tags['pop']].size + 1
+            populations[cell.tags['pop']].defaultValue.elements.append(ArrayElement(index=len(populations[cell.tags['pop']].defaultValue.elements) , position=Point(x=float(cell.tags['x']), y=float(cell.tags['y']), z=float(cell.tags['z']))))
+            
     def extractInstances(self, netpyne_model, netpyne_geppetto_library, geppetto_model):
         instance = pygeppetto.Variable(id='network')
         instance.types.append(netpyne_geppetto_library.types[0])
