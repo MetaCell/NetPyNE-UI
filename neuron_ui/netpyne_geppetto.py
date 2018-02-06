f6c2d6735d9225571b949acbf4ec568f37df2749
Adrian Quintana <adrianquintana@gmail.com>
2018-02-06 23:18:11 +0000

    remove netpyne_init and fix duplicated model

--- neuron_ui/netpyne_geppetto.py
+++ neuron_ui/netpyne_geppetto.py
@@ -2,20 +2,21 @@
 GeppettoNeuron.py
 Initialise geppetto neuron, listeners and variables
 """
-import logging
-import threading
-import time
 import StringIO
 import json
 import os
 import importlib
 import sys
 import subprocess
+import logging
+import threading
+import time
 
-from jupyter_geppetto.geppetto_comm import GeppettoJupyterModelSync
-from jupyter_geppetto.geppetto_comm import GeppettoJupyterGUISync
+
 from neuron_ui import neuron_utils
-from neuron_ui.netpyne_init import netParams, simConfig, tests, metadata, api, sim, analysis, specs
+# from neuron_ui.netpyne_init import netParams, simConfig, tests, metadata, api, sim, analysis, specs
+from netpyne import specs, sim, analysis
+from netpyne.metadata import metadata, api
 from netpyne_model_interpreter import NetPyNEModelInterpreter
 from model.model_serializer import GeppettoModelSerializer
 import matplotlib.pyplot as plt
@@ -25,78 +26,9 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib.figure import Figure
 import neuron
 from shutil import copyfile
-# TODO we should probablye first import something generic for geppetto which brings in the timer, the global handler, etc
+from jupyter_geppetto.geppetto_comm import GeppettoJupyterModelSync, GeppettoJupyterGUISync
 
 
-class LoopTimer(threading.Thread):
-    """
-    a Timer that calls f every interval
-
-    A thread that checks all the variables that we are synching between Python and Javascript and if 
-    these variables have changed on the Python side will propagate the changes to Javascript
-
-    TODO This code should move to a generic geppetto class since it's not NetPyNE specific
-    """
-
-    def __init__(self, interval, fun=None):
-        """
-        @param interval: time in seconds between call to fun()
-        @param fun: the function to call on timer update
-        """
-        self.started = False
-        self.interval = interval
-        if fun == None:
-            fun = self.process_events
-        self.fun = fun
-        threading.Thread.__init__(self)
-        self.setDaemon(True)
-
-    def run(self):
-        # TODO With this line it hangs in some setups. Figure out if it's needed
-        # h.nrniv_bind_thread(threading.current_thread().ident);
-        self.started = True
-        while True:
-            self.fun()
-            time.sleep(self.interval)
-
-    def process_events(self):
-        # h.doEvents()
-        # h.doNotify()
-
-        try:
-            # Using 'list' so that a copy is made and we don't get: dictionary changed size during iteration items
-            for key, value in list(GeppettoJupyterModelSync.record_variables.items()):
-                value.timeSeries = key.to_python()
-
-            for model, synched_component in list(GeppettoJupyterGUISync.synched_models.items()):
-                if model != '':
-                    try:
-                        modelValue = eval(model)
-                    except KeyError:
-                        logging.debug("Error evaluating "+model)
-                        exec(model + "= ''")
-                        modelValue = eval(model)
-
-                synched_component.value = json.dumps(modelValue)
-
-        except Exception as exception:
-            logging.exception(
-                "Error on Sync Mechanism for non-sim environment thread")
-            raise
-
-def globalMessageHandler(identifier, command, parameters):
-    """
-    TODO This code should move to a generic geppetto class since it's not NetPyNE specific
-    """
-    logging.debug('Global Message Handler')
-    logging.debug(command)
-    logging.debug(parameters)
-    if parameters == '':
-        response = eval(command)
-    else:
-        response = eval(command + '(*parameters)')
-    GeppettoJupyterModelSync.events_controller.triggerEvent(
-        "receive_python_message", {'id': identifier, 'response': response})
 
 class NetPyNEGeppetto():
 
@@ -253,37 +185,120 @@ class NetPyNEGeppetto():
         svgs = []
         svgs.append(ui.getSVG(fig))
         return svgs
+class LoopTimer(threading.Thread):
+    """
+    a Timer that calls f every interval
+
+    A thread that checks all the variables that we are synching between Python and Javascript and if 
+    these variables have changed on the Python side will propagate the changes to Javascript
+
+    TODO This code should move to a generic geppetto class since it's not NetPyNE specific
+    """
+
+    def __init__(self, interval, fun=None):
+        """
+        @param interval: time in seconds between call to fun()
+        @param fun: the function to call on timer update
+        """
+        self.started = False
+        self.interval = interval
+        if fun == None:
+            fun = self.process_events
+        self.fun = fun
+        threading.Thread.__init__(self)
+        self.setDaemon(True)
+
+    def run(self):
+        # TODO With this line it hangs in some setups. Figure out if it's needed
+        # h.nrniv_bind_thread(threading.current_thread().ident);
+        self.started = True
+        while True:# from neuron_ui import neuron_utils
+            self.fun()
+            time.sleep(self.interval)
+
+    def process_events(self):
+        # h.doEvents()
+        # h.doNotify()
+
+        try:
+            # Using 'list' so that a copy is made and we don't get: dictionary changed size during iteration items
+            for key, value in list(GeppettoJupyterModelSync.record_variables.items()):
+                value.timeSeries = key.to_python()
+
+            for model, synched_component in list(GeppettoJupyterGUISync.synched_models.items()):
+                if model != '':
+                    try:
+                        modelValue = eval(model)
+                    except KeyError:
+                        logging.debug("Error evaluating "+model)
+                        exec(model + "= ''")
+                        modelValue = eval(model)
+
+                synched_component.value = json.dumps(modelValue)
+
+        except Exception as exception:
+            logging.exception(
+                "Error on Sync Mechanism for non-sim environment thread")
+            raise
+
+
+def globalMessageHandler(identifier, command, parameters):
+    """
+    TODO This code should move to a generic geppetto class since it's not NetPyNE specific
+    """
+    logging.debug('Global Message Handler')
+    logging.debug(command)
+    logging.debug(parameters)
+    if parameters == '':
+        response = eval(command)
+    else:
+        response = eval(command + '(*parameters)')
+    GeppettoJupyterModelSync.events_controller.triggerEvent(
+        "receive_python_message", {'id': identifier, 'response': response})
+
+def GeppettoInit():      
+    try:
+        # Configure log
+        neuron_utils.configure_logging()
+
+        logging.debug('Initialising NetPyNE')
+
+        # Reset any previous value
+        logging.debug('Initialising Sync and Status Variables')
+        # GeppettoJupyterGUISync.sync_values = defaultdict(list)
+        # GeppettoJupyterModelSync.record_variables = defaultdict(list)
+        GeppettoJupyterModelSync.current_project = None
+        GeppettoJupyterModelSync.current_experiment = None
+        GeppettoJupyterModelSync.current_model = None
+        GeppettoJupyterModelSync.current_python_model = None
+        GeppettoJupyterModelSync.events_controller = GeppettoJupyterModelSync.EventsSync()
+        GeppettoJupyterModelSync.events_controller.register_to_event(
+            [GeppettoJupyterModelSync.events_controller._events['Global_message']], globalMessageHandler)
+
+        # Sync values when no sim is running
+        logging.debug('Initialising Sync Mechanism for non-sim environment')
+        timer = LoopTimer(0.3)
+        timer.start()
+        while not timer.started:
+            time.sleep(0.001)
+
         
 
 
-try:
-    # Configure log
-    neuron_utils.configure_logging()
-
-    logging.debug('Initialising NetPyNE')
-
-    # Reset any previous value
-    logging.debug('Initialising Sync and Status Variables')
-    # GeppettoJupyterGUISync.sync_values = defaultdict(list)
-    # GeppettoJupyterModelSync.record_variables = defaultdict(list)
-    GeppettoJupyterModelSync.current_project = None
-    GeppettoJupyterModelSync.current_experiment = None
-    GeppettoJupyterModelSync.current_model = None
-    GeppettoJupyterModelSync.current_python_model = None
-    GeppettoJupyterModelSync.events_controller = GeppettoJupyterModelSync.EventsSync()
-    GeppettoJupyterModelSync.events_controller.register_to_event(
-        [GeppettoJupyterModelSync.events_controller._events['Global_message']], globalMessageHandler)
-
-    # Sync values when no sim is running
-    logging.debug('Initialising Sync Mechanism for non-sim environment')
-    timer = LoopTimer(0.3)
-    timer.start()
-    while not timer.started:
-        time.sleep(0.001)
+    except Exception as exception:
+        logging.exception("Unexpected error in neuron_geppetto initialization:")
+        logging.error(exception)
 
 
-    netpyne_geppetto = NetPyNEGeppetto()
+GeppettoInit()
+netpyne_geppetto = NetPyNEGeppetto()
 
-except Exception as exception:
-    logging.exception("Unexpected error in neuron_geppetto initialization:")
-    logging.error(exception)
+netParams = specs.NetParams()
+simConfig = specs.SimConfig()
+
+neuron_utils.createProject(name='SampleProject')
+
+GeppettoJupyterModelSync.current_model.original_model = json.dumps({'netParams': netParams.__dict__,
+                                                                    'simConfig': simConfig.__dict__,
+                                                                    'metadata': metadata.metadata,
+                                                                    'requirement': 'from neuron_ui.netpyne_geppetto import *'})
