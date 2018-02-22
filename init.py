ad5f2e70d6f1bf6e1c55e55855d832007d454105
Adrian Quintana <adrianquintana@gmail.com>
2018-02-22 10:18:32 +0000

    remove copied template and add to gitignore

--- init.py
+++
@@ -1,8 +0,0 @@
-from netpyne import sim
-netParams = sim.loadNetParams("./netParams.json", None, False)
-simConfig = sim.loadSimCfg("./simParams.json", None, False)
-
-sim.createSimulateAnalyze(netParams, simConfig)
-
-
-
