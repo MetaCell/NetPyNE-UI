f7e6bc3a85cc054da2e6cf328f8851b4fd243871
rodriguez-facundo <bruseghini.f@gmail.com>
2020-03-08 13:17:47 -0300

    Keep WEB-INFO folder

--- webapp/components/settings/actions/LoadFile.js
+++ webapp/components/settings/actions/LoadFile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { List } from '@material-ui/core/List';
+import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import { orange, grey } from '@material-ui/core/colors';
