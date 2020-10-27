79b5ee82800fc471db6345aa3d1d936c220b487e
Vidhya Longani <vidhya.longani@kiwitech.com>
2020-10-27 22:24:11 +0530

    #179 Edit view - replace the question marks, tooltip over the textfields, plus icon in the start

--- webapp/components/general/List.js
+++ webapp/components/general/List.js
@@ -1,24 +1,26 @@
-import React, { Component } from 'react';
-import TextField from '@material-ui/core/TextField';
-import IconButton from '@material-ui/core/IconButton';
+import React, { Component } from "react";
+import TextField from "@material-ui/core/TextField";
+import IconButton from "@material-ui/core/IconButton";
+import Add from "@material-ui/icons/Add";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Tooltip from "./Tooltip";
+import Chip from "@material-ui/core/Chip";
+import Grid from "@material-ui/core/Grid";
+import NetPyNEIcon from "../general/NetPyNEIcons";
+import Box from "@material-ui/core/Box";
+import { withStyles } from "@material-ui/core/styles";
 
-import Tooltip from './Tooltip'
-import Chip from '@material-ui/core/Chip'
-import Grid from '@material-ui/core/Grid'
-import NetPyNEIcon from '../general/NetPyNEIcons';
-import Box from '@material-ui/core/Box'
-import { withStyles } from '@material-ui/core/styles'
 /**
  * Generic List/Dict Component
  */
 class ListComponent extends Component {
-
   constructor (props) {
     super(props);
     this.state = {
       model: props.model,
-      children: (props.realType == 'dict' || props.realType == 'dict(dict)') ? {} : [],
-      newItemValue: ''
+      children:
+        props.realType == "dict" || props.realType == "dict(dict)" ? {} : [],
+      newItemValue: "",
     };
     this.addChild = this.addChild.bind(this);
     this.handleNewItemChange = this.handleNewItemChange.bind(this);
@@ -26,59 +28,77 @@ class ListComponent extends Component {
 
   componentDidMount () {
     // this.props.value is populated from pythonControlledCapability
-    if (this.props.default && this.isDict() && this.props.value === '') {
+    if (this.props.default && this.isDict() && this.props.value === "") {
       // this.props.onChange(null, null, this.props.default);
     }
-    
   }
 
   isDict () {
-    return this.props.realType == 'dict' || this.props.realType == 'dict(dict)'
+    return this.props.realType == "dict" || this.props.realType == "dict(dict)";
   }
 
   isValid (value) {
     switch (this.props.realType) {
-    case 'list(float)':
-      var valid = (value.match(/^-?\d*(\.\d+)?$/));
+    case "list(float)":
+      var valid = value.match(/^-?\d*(\.\d+)?$/);
       break;
-    case 'list(list(float))':
+    case "list(list(float))":
       var valid = true;
-      value.split(',').forEach(element => {
+      value.split(",").forEach(element => {
         if (!element.match(/^-?\d*(\.\d+)?$/)) {
           valid = false;
         }
       });
-      if (value.endsWith(',')) {
+      if (value.endsWith(",")) {
         valid = false;
       }
       break;
-    case 'dict':
-      var valid = (value.match(/:/g) || []).length == 1 && !value.startsWith(':') && !value.endsWith(':');
+    case "dict":
+      var valid
+          = (value.match(/:/g) || []).length == 1
+          && !value.startsWith(":")
+          && !value.endsWith(":");
       break;
-    case 'dict(dict)':
+    case "dict(dict)":
       var valid = true;
-      var value = this.state.newItemValue.replace(/[ "']/g, '');
+      var value = this.state.newItemValue.replace(/[ "']/g, "");
 
       if ((value.match(/;/g) || []).length != 0) {
         valid = false;
       } else {
-        if ((value.match(/{/g) || []).length != 1 || (value.match(/}/g) || []).length != 1) {
+        if (
+          (value.match(/{/g) || []).length != 1
+            || (value.match(/}/g) || []).length != 1
+        ) {
           valid = false;
         } else {
-          if (value.indexOf('{') > value.indexOf('}') || !value.endsWith('}')) {
+          if (
+            value.indexOf("{") > value.indexOf("}")
+              || !value.endsWith("}")
+          ) {
             valid = false;
           } else {
             var subDict = value.match(/\{(.*?)\}/)[1];
-            if ((subDict.match(/:/g) || []).length - 1 != (subDict.match(/,/g) || []).length) {
+            if (
+              (subDict.match(/:/g) || []).length - 1
+                != (subDict.match(/,/g) || []).length
+            ) {
               valid = false;
             } else {
-              subDict.split(',').forEach(element => {
-                if ((element.match(/:/g) || []).length != 1 || element.startsWith(':') || element.endsWith(':')) {
+              subDict.split(",").forEach(element => {
+                if (
+                  (element.match(/:/g) || []).length != 1
+                    || element.startsWith(":")
+                    || element.endsWith(":")
+                ) {
                   valid = false;
                 }
               });
-              var reminder = value.replace('{' + subDict + '}', '');
-              if ((reminder.match(/:/g) || []).length != 1 || !reminder.endsWith(':')) {
+              var reminder = value.replace("{" + subDict + "}", "");
+              if (
+                (reminder.match(/:/g) || []).length != 1
+                  || !reminder.endsWith(":")
+              ) {
                 valid = false;
               }
             }
@@ -95,54 +115,64 @@ class ListComponent extends Component {
 
   getErrorMessage () {
     switch (this.props.realType) {
-    case 'list(float)':
-      var message = 'Only float numbers are allowed.';
+    case "list(float)":
+      var message = "Only float numbers are allowed.";
       break;
-    case 'list(list(float))':
-      var message = 'Only comma separated float numbers are allowed.';
+    case "list(list(float))":
+      var message = "Only comma separated float numbers are allowed.";
       break;
-    case 'dict':
-      var message = 'Key:Value pairs must be separated by colon : ';
+    case "dict":
+      var message = "Key:Value pairs must be separated by colon : ";
       break;
-    case 'dict(dict)':
-      var message = 'Incorrect format. Example -> v_soma : { sec: soma, loc: 0.5, var: v}';
-      break
+    case "dict(dict)":
+      var message
+          = "Incorrect format. Example -> v_soma : { sec: soma, loc: 0.5, var: v}";
+      break;
     default:
-      var message = 'No a valid value';
+      var message = "No a valid value";
       break;
     }
     return message;
   }
 
   handleNewItemChange (event) {
-    this.setState({ newItemValue: event.target.value, newItemErrorText: '' })
+    this.setState({ newItemValue: event.target.value, newItemErrorText: "" });
   }
 
   addChild () {
-    if (this.state.newItemValue != '' && this.isValid(this.state.newItemValue)) {
+    if (
+      this.state.newItemValue != ""
+      && this.isValid(this.state.newItemValue)
+    ) {
       var children = this.state.children;
       switch (this.props.realType) {
-      case 'list(list(float))':
-        var newValue = '[' + this.state.newItemValue + ']';
+      case "list(list(float))":
+        var newValue = "[" + this.state.newItemValue + "]";
         children.push(newValue);
         break;
-      case 'dict':
-        var newValue = this.state.newItemValue.split(':').map(entry => entry);
+      case "dict":
+        var newValue = this.state.newItemValue
+          .split(":")
+          .map(entry => entry);
         if (!isNaN(newValue[1])) {
-          newValue[1] = parseFloat(newValue[1])
+          newValue[1] = parseFloat(newValue[1]);
         }
         children[newValue[0]] = newValue[1];
         break;
-      case 'dict(dict)':
-        var key = this.state.newItemValue.split(':')[0].replace(/[ "']/g, '');
-        children[key] = {}
-        var newValue = this.state.newItemValue.match(/\{(.*?)\}/)[1].replace(/[ "']/g, '').split(',').map(entry => entry.split(':'));
+      case "dict(dict)":
+        var key = this.state.newItemValue.split(":")[0].replace(/[ "']/g, "");
+        children[key] = {};
+        var newValue = this.state.newItemValue
+          .match(/\{(.*?)\}/)[1]
+          .replace(/[ "']/g, "")
+          .split(",")
+          .map(entry => entry.split(":"));
         newValue.forEach(entry => {
           if (!isNaN(entry[1])) {
-            entry[1] = parseFloat(entry[1])
+            entry[1] = parseFloat(entry[1]);
           }
-          children[key][entry[0]] = entry[1]
-        })
+          children[key][entry[0]] = entry[1];
+        });
         break;
       default:
         var newValue = this.state.newItemValue;
@@ -152,14 +182,13 @@ class ListComponent extends Component {
       // Call to conversion function
       this.convertToPython(children);
     } else {
-      this.setState({ newItemErrorText: this.getErrorMessage() })
+      this.setState({ newItemErrorText: this.getErrorMessage() });
     }
   }
 
   removeChild (childIndex) {
     var children = this.state.children;
-    if (this.props.realType == 'dict' || this.props.realType == 'dict(dict)') {
-
+    if (this.props.realType == "dict" || this.props.realType == "dict(dict)") {
       delete children[childIndex];
     } else {
       children.splice(childIndex, 1);
@@ -170,17 +199,17 @@ class ListComponent extends Component {
 
   convertToPython (children) {
     // Update State
-    this.setState({ children: children, newItemValue: '' });
+    this.setState({ children: children, newItemValue: "" });
 
-    if (this.props.realType == 'dict' || this.props.realType == 'dict(dict)') {
+    if (this.props.realType == "dict" || this.props.realType == "dict(dict)") {
       var newValue = children;
     } else {
       var newValue = this.state.children.map((child, i) => {
         switch (this.props.realType) {
-        case 'list(float)':
-          var childConverted = parseFloat(child)
+        case "list(float)":
+          var childConverted = parseFloat(child);
           break;
-        case 'list(list(float))':
+        case "list(list(float))":
           var childConverted = JSON.parse(child);
           break;
         default:
@@ -188,78 +217,110 @@ class ListComponent extends Component {
           break;
         }
         return childConverted;
-      })
+      });
     }
 
-    if (newValue != undefined && this.state.value != newValue && this.props.onChange != undefined) {
+    if (
+      newValue != undefined
+      && this.state.value != newValue
+      && this.props.onChange != undefined
+    ) {
       this.props.onChange(null, null, newValue);
     }
   }
 
   convertFromPython (prevProps, prevState, value) {
-    if (value != undefined && prevProps.value != value && value != '') {
-      if (this.props.realType == 'dict' || this.props.realType == 'dict(dict)') {
-        return (typeof value == 'string') ? JSON.parse(value) : value;
+    if (value != undefined && prevProps.value != value && value != "") {
+      if (
+        this.props.realType == "dict"
+        || this.props.realType == "dict(dict)"
+      ) {
+        return typeof value == "string" ? JSON.parse(value) : value;
       } else {
         if (!Array.isArray(value)) {
           value = [value];
         }
-        return value.map((child, i) => (typeof child == 'string') ? child : JSON.stringify(child));
+        return value.map((child, i) =>
+          typeof child == "string" ? child : JSON.stringify(child)
+        );
       }
-
     }
   }
 
   componentDidUpdate (prevProps, prevState) {
-    var newValue = this.convertFromPython(prevProps, prevState, this.props.value);
+    var newValue = this.convertFromPython(
+      prevProps,
+      prevState,
+      this.props.value
+    );
     if (newValue != undefined) {
       this.setState({ children: newValue });
     }
   }
 
   render () {
-    var childrenWithExtraProp = Object.keys(this.state.children).map((key, index) => {
-      key = key.toString();
-      if (this.props.realType == 'dict') {
-        var value = key + ' : ' + JSON.stringify(this.state.children[key]);
-      } else if (this.props.realType == 'dict(dict)') {
-        var value = key + ':   ' + JSON.stringify(this.state.children[key]).replace(/["']/g, '').replace(/[:]/g, ': ').replace(/[,]/g, ', ');
-      } else {
-        var value = this.state.children[key];
+    var childrenWithExtraProp = Object.keys(this.state.children).map(
+      (key, index) => {
+        key = key.toString();
+        if (this.props.realType == "dict") {
+          var value = key + " : " + JSON.stringify(this.state.children[key]);
+        } else if (this.props.realType == "dict(dict)") {
+          var value
+            = key
+            + ":   "
+            + JSON.stringify(this.state.children[key])
+              .replace(/["']/g, "")
+              .replace(/[:]/g, ": ")
+              .replace(/[,]/g, ", ");
+        } else {
+          var value = this.state.children[key];
+        }
+        return (
+          <Chip
+            key={key}
+            label={value}
+            style={{ marginRight: 8 }}
+            onDelete={() => this.removeChild(key)}
+            color="primary"
+          />
+        );
       }
-      return <Chip key={key} label={value} style={{ marginRight: 8 }} onDelete={() => this.removeChild(key)} color="primary" />
-    });
+    );
 
-    const { classes } = this.props
+    const { classes } = this.props;
     return (
-      <Box >
-        <TextField
-          variant="filled"
-          id={this.props.id}
-          label={this.props.label ? 'Add new ' + this.props.label : 'Add new item'}
-          onChange={this.handleNewItemChange}
-          onKeyPress={e => e.key === 'Enter' ? this.addChild() : null }
-          value={this.state.newItemValue}
-          fullWidth
-          helperText={this.state.newItemErrorText}
-          InputProps={{
-            endAdornment: (
-              <Tooltip title="Add item to the list" placement="top">
-                <IconButton size="small" onClick={this.addChild}>
-                  <NetPyNEIcon name="add"/>
-                </IconButton>
-              </Tooltip> 
-            )
-          }}
-        />
+      <Box>
+        <Grid container spacing={1} alignItems="flex-end">
+          <Grid item>
+            <Box display="flex" alignItems="center" className={classes.addIcon}><IconButton><Add onClick={this.addChild}/></IconButton></Box>
+            <TextField
+              variant="filled"
+              id={this.props.id}
+              label={
+                this.props.label ? "Add new " + this.props.label : "Add new item"
+              }
+              onChange={this.handleNewItemChange}
+              onKeyPress={e => (e.key === "Enter" ? this.addChild() : null)}
+              value={this.state.newItemValue}
+              fullWidth
+              helperText={this.state.newItemErrorText}
+              className="field-start-icon"
+            />
+          </Grid>
+        </Grid>
         <Box m={1}>{childrenWithExtraProp}</Box>
-
       </Box>
-    )
+    );
   }
 }
 
+const styles = () => ({
+  addButton: { height: 48 },
+  addIcon: {
+    backgroundColor: "rgba(255, 255, 255, 0.09)",
+    borderTopLeftRadius: "4px",
+    height: '55.09px'
+  }
+});
 
-const styles = () => ({ addButton: { height: 48 } })
-
-export default withStyles(styles)(ListComponent)
+export default withStyles(styles)(ListComponent);
