ea66375c83e55b1198da39d0da3956f4e597694d
vidhya-metacell <71261494+vidhya-metacell@users.noreply.github.com>
2021-03-23 14:01:59 +0530

    #229 Map api return with the UI

--- webapp/components/experiments/Experiments.js
+++ webapp/components/experiments/Experiments.js
@@ -1,6 +1,4 @@
 import React, { useEffect } from 'react';
-import TextField from '@material-ui/core/TextField';
-// import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -11,7 +9,6 @@ import Paper from '@material-ui/core/Paper';
 import DeleteIcon from '@material-ui/icons/Delete';
 import ReplayIcon from '@material-ui/icons/Replay';
 import EditIcon from '@material-ui/icons/Edit';
-import FaceIcon from '@material-ui/icons/Face';
 import Chip from '@material-ui/core/Chip';
 import CancelRoundedIcon from '@material-ui/icons/CancelRounded';
 import FileCopyOutlinedIcon from '@material-ui/icons/FileCopyOutlined';
@@ -64,157 +61,54 @@ const Experiments = (props) => {
   };
 
   return (
-    // <div>
-    //   <TextField />
-    //   <Button onClick={editExperiment} />
-    //   <Button onClick={cleanExperiment} />
-    //   <Button onClick={deleteExperiment} />
-    //   <Button onClick={viewExperiment} />
-    //   {
-    //     experiments.map((experiment) => <div>{experiment.name}</div>)
-    //   }
-    // </div>
     <GridLayout>
       <div>
         <Box>
           <TableContainer component={Paper} className="experimentTableContainer">
             <Table aria-label="simple table" className="experimentTable">
               <TableBody>
-                <TableRow>
-                  <TableCell component="th" scope="row">
-                    <Button>
-                      <Typography variant="h6" className="experimentHead">
-                        Experiment W
+                { experiments.map((experiment) => (
+                  <TableRow key={experiment?.name}>
+                    <TableCell component="th" scope="row">
+                      <Button>
+                        <Typography variant="h6" className="experimentHead">
+                          {experiment?.name}
+                        </Typography>
+                        <ChevronRightIcon className="experimentHeadIcon" />
+                      </Button>
+                    </TableCell>
+                    <TableCell align="left">
+                      <Typography variant="h6" className="experimentDate">
+                        28.03.2021
                       </Typography>
-                      <ChevronRightIcon className="experimentHeadIcon" />
-                    </Button>
-                  </TableCell>
-                  <TableCell align="left">
-                    <Typography variant="h6" className="experimentDate">
-                      28.03.2021
-                    </Typography>
-                  </TableCell>
-                  <TableCell align="left" className="experimentTableCell">
-                    <Chip
-                      label="DESIGN"
-                      onClick={handleClick}
-                    />
-                  </TableCell>
-                  <TableCell align="right">
-                    <Button className="experimentIcon"><EditIcon/></Button>
-                  </TableCell>
-                  <TableCell align="center">
-                    <Divider orientation="vertical" />
-                  </TableCell>
-                  <TableCell align="right">
-                    <Button className="experimentIcon"><ReplayIcon/></Button>
-                  </TableCell>
-                  <TableCell align="right">
-                    <Button className="experimentIcon"><DeleteIcon/></Button>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell component="th" scope="row">
-                    <Button>
-                      <Typography variant="h6" className="experimentHead">
-                        Experiment W
-                      </Typography>
-                      <ChevronRightIcon className="experimentHeadIcon"/>
-                    </Button>
-                  </TableCell>
-                  <TableCell align="left">
-                    <Typography variant="h6" className="experimentDate">
-                      28.03.2021
-                    </Typography>
-                  </TableCell>
-                  <TableCell align="left" className="experimentTableCell">
-                    <Chip
-                      icon={<FaceIcon />}
-                      label="SIMULATING"
-                      onClick={handleClick}
-                      onDelete={handleDelete}
-                      deleteIcon={<CancelRoundedIcon />}
-                    />
-                  </TableCell>
-                  <TableCell align="right">
-                    <Button className="experimentIcon"><FileCopyOutlinedIcon/></Button>
-                  </TableCell>
-                  <TableCell align="center">
-                    <Divider orientation="vertical" />
-                  </TableCell>
-                  <TableCell align="right">
-                    <Button className="experimentIcon"><ReplayIcon/></Button>
-                  </TableCell>
-                  <TableCell align="right">
-                    <Button className="experimentIcon"><DeleteIcon/></Button>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell component="th" scope="row">
-                    <Button>
-                      <Typography variant="h6" className="experimentHead">
-                        Experiment W
-                      </Typography>
-                      <ChevronRightIcon className="experimentHeadIcon"/>
-                    </Button>
-                  </TableCell>
-                  <TableCell align="left">
-                    <Typography variant="h6" className="experimentDate">
-                      28.03.2021
-                    </Typography>
-                  </TableCell>
-                  <TableCell align="left" className="experimentTableCell">
-                    <Chip
-                      label="DESIGN"
-                      onClick={handleClick}
-                    />
-                  </TableCell>
-                  <TableCell align="right">
-                    <Button className="experimentIcon"><EditIcon/></Button>
-                  </TableCell>
-                  <TableCell align="center">
-                    <Divider orientation="vertical" />
-                  </TableCell>
-                  <TableCell align="right">
-                    <Button className="experimentIcon"><ReplayIcon/></Button>
-                  </TableCell>
-                  <TableCell align="right">
-                    <Button className="experimentIcon"><DeleteIcon/></Button>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell component="th" scope="row">
-                    <Button>
-                      <Typography variant="h6" className="experimentHead">
-                        Experiment W
-                      </Typography>
-                      <ChevronRightIcon className="experimentHeadIcon"/>
-                    </Button>
-                  </TableCell>
-                  <TableCell align="left">
-                    <Typography variant="h6" className="experimentDate">
-                      28.03.2021
-                    </Typography>
-                  </TableCell>
-                  <TableCell align="left" className="experimentTableCell">
-                    <Chip
-                      label="DESIGN"
-                      onClick={handleClick}
-                    />
-                  </TableCell>
-                  <TableCell align="right">
-                    <Button className="experimentIcon"><EditIcon/></Button>
-                  </TableCell>
-                  <TableCell align="center">
-                    <Divider orientation="vertical" />
-                  </TableCell>
-                  <TableCell align="right">
-                    <Button className="experimentIcon"><ReplayIcon/></Button>
-                  </TableCell>
-                  <TableCell align="right">
-                    <Button className="experimentIcon"><DeleteIcon/></Button>
-                  </TableCell>
-                </TableRow>
+                    </TableCell>
+                    <TableCell align="left" className="experimentTableCell">
+                      {experiment?.state !== 'SIMULATING' ? 
+                        <Chip
+                          label={experiment?.state}
+                        /> :
+                        <Chip
+                          icon={<Box className="MuiChipLoader"></Box>}
+                          label="SIMULATING"
+                          onDelete={handleDelete}
+                          deleteIcon={<CancelRoundedIcon />}
+                        />
+                      }
+                    </TableCell>
+                    <TableCell align="right">
+                      <Button className="experimentIcon" onClick={viewExperiment}>{experiment?.state === 'DESIGN' ? <EditIcon/> :  <FileCopyOutlinedIcon/>}</Button>
+                    </TableCell>
+                    <TableCell align="center">
+                      <Divider orientation="vertical" />
+                    </TableCell>
+                    <TableCell align="right">
+                      <Button className="experimentIcon" onClick={cleanExperiment}><ReplayIcon/></Button>
+                    </TableCell>
+                    <TableCell align="right">
+                      <Button className="experimentIcon" onClick={deleteExperiment}><DeleteIcon/></Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
               </TableBody>
             </Table>
           </TableContainer>
