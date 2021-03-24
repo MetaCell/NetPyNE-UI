7fc26778ee5af4afb8333e2f3da8c10280adc177
vidhya-metacell <71261494+vidhya-metacell@users.noreply.github.com>
2021-03-24 18:26:05 +0530

    #229 Moved styles to component using withStyles

--- webapp/components/experiments/Experiments.js
+++ webapp/components/experiments/Experiments.js
@@ -17,13 +17,96 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import AddIcon from '@material-ui/icons/Add';
-
 import {
   GridLayout,
 } from 'netpyne/components';
 import {
   EXPERIMENT_STATE
 } from '../../constants';
+import { withStyles } from '@material-ui/core/styles';
+
+const useStyles = theme => ({
+  root: {
+    '& .MuiTableContainer-root': {
+      '&::-webkit-scrollbar': {
+        height: theme.spacing(1),
+      },
+      '& .MuiTable-root': {
+        '& .experimentHead': {
+          fontSize: '1rem',
+        },
+        '& .experimentHeadIcon': {
+          fontSize: '1rem',
+        },
+        '& .experimentDate': {
+          fontStyle: 'italic',
+          fontWeight: 300,
+          fontSize: '1rem',
+        },
+        '& .experimentIcon': {
+          minWidth: 'auto',
+          '& .MuiSvgIcon-root': {
+            fontSize: '1.2rem',
+          }
+        },
+        '& .MuiChip-label': {
+          fontSize:' 0.77rem',
+          fontWeight: 600,
+        },
+        '& .MuiChip-root': {
+          background: 'rgba(255, 255, 255, 0.1)',
+          height: theme.spacing(3.2),
+        },
+        '& .MuiChip-deleteIcon': {
+          marginRight: theme.spacing(0.4),
+        },
+        '& .MuiTableCell-root': {
+          padding: theme.spacing(1, 2),
+          '&:nth-child(1)': {
+            width: '18.75rem',
+          },
+          '&:nth-child(2)': {
+            width: '9.375rem',
+          },
+          '&:nth-child(6)': {
+            '& .MuiButton-label': {
+              transform: 'rotate(-65deg)',
+            }
+          }
+        },
+        '& .MuiChip-icon': {
+          color: '#A8A5A5',
+        },
+        '& .MuiDivider-vertical': {
+          height: theme.spacing(4),
+          background: 'rgba(255, 255, 255, 0.3)',
+        },
+        '& .MuiTableCell-alignRight': {
+          width: theme.spacing(6),
+          paddingLeft: 0,
+        },
+        '& .MuiTableCell-alignCenter': {
+          width: theme.spacing(6),
+        }
+      },
+      '& .MuiChipLoader': {
+        border: '3px solid rgba(196, 196, 196, 0.3)',
+        borderRadius: '50%',
+        borderTop: '3px solid #A8A5A5',
+        width: theme.spacing(2.4),
+        height: theme.spacing(2.4),
+        animation: 'spin 2s linear infinite',
+      }
+    },
+    '& .MuiBox-root': {
+      '&.experimentFooter': {
+        position: 'absolute',
+        bottom: theme.spacing(2.4),
+        left: 'auto',
+      }
+    }
+  },
+});
 
 /**
  * Lists all Experiments.
@@ -37,6 +120,7 @@ const Experiments = (props) => {
     experiments,
     getExperiments,
     editExperiment,
+    classes,
   } = props;
 
   useEffect(() => {
@@ -67,13 +151,12 @@ const Experiments = (props) => {
     const date = new Date(timestamp)
     return date?.toLocaleDateString();
   }
-
   return (
-    <GridLayout>
+    <GridLayout className={classes.root}>
       <div>
         <Box>
-          <TableContainer component={Paper} className="experimentTableContainer">
-            <Table aria-label="simple table" className="experimentTable">
+          <TableContainer component={Paper}>
+            <Table aria-label="simple table">
               <TableBody>
                 { experiments.map((experiment) => (
                   <TableRow key={experiment?.name}>
@@ -141,4 +224,4 @@ const Experiments = (props) => {
   );
 };
 
-export default Experiments;
+export default withStyles(useStyles)(Experiments);
