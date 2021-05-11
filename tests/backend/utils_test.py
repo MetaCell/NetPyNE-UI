28e723efe15fe28281b4e4642ec6b4801c6d390a
Filippo Ledda <filippo@metacell.us>
2021-05-11 11:28:40 +0200

    #277 Added unit test for double mechanism reloading

---
+++ tests/backend/utils_test.py
@@ -0,0 +1,10 @@
+import os
+
+from netpyne_ui import mod_utils
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+def test_compile_mod():
+    mod_utils.compileModMechFiles(True, os.path.join(HERE, 'mod'))
+    mod_utils.compileModMechFiles(True, os.path.join(HERE, 'mod'))  # Will be ignored by neuron thanks to neuron.
+
+    mod_utils.compileModMechFiles(True, os.path.join(HERE, './mod')) # this is enough to fool Neuron
