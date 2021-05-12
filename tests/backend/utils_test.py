50dfeb75644aa8975726e2a921572b49826effd4
Filippo Ledda <filippo@metacell.us>
2021-05-12 10:21:31 +0200

    #277 fix/clean tests

--- tests/backend/utils_test.py
+++ tests/backend/utils_test.py
@@ -5,6 +5,6 @@ HERE = os.path.dirname(os.path.abspath(__file__))
 
 def test_compile_mod():
     mod_utils.compileModMechFiles(True, os.path.join(HERE, 'mod'))
-    mod_utils.compileModMechFiles(True, os.path.join(HERE, 'mod'))  # Will be ignored by neuron thanks to neuron.
+    mod_utils.compileModMechFiles(True, os.path.join(HERE, 'mod'))  # Will be ignored by neuron thanks to neuron.nrn_dll_loaded
 
-    mod_utils.compileModMechFiles(True, os.path.join(HERE, './mod')) # this is enough to fool Neuron
+    # mod_utils.compileModMechFiles(True, os.path.join(HERE, './mod')) # this is enough to fool Neuron
