5323e2c3488e8b9a754ab6722ff092e27385eaee
Lucas Rebscher <lucas@metacell.us>
2021-06-30 12:42:07 +0200

    #342 Add mod file support to template scripts

--- netpyne_ui/templates/batch_run_single.py
+++ netpyne_ui/templates/batch_run_single.py
@@ -1,4 +1,9 @@
+import neuron
 from netpyne import sim
+
+# Folder that contains x86_64 folder
+NETPYNE_WORKDIR_PATH = '../../../'
+neuron.load_mechanisms(NETPYNE_WORKDIR_PATH)
 
 # read cfg and netParams from command line arguments if available; otherwise use default
 simConfig, netParams = sim.readCmdLineArgs(simConfigDefault='cfg.py', netParamsDefault='netParams.py')
--- netpyne_ui/templates/run.py
+++ netpyne_ui/templates/run.py
@@ -1,20 +1,25 @@
 import os
 import json
+import neuron
 
 from netpyne import sim
 
 os.chdir(os.path.dirname(__file__))
 
+# Folder that contains x86_64 folder
+NETPYNE_WORKDIR_PATH = '../../../'
+neuron.load_mechanisms(NETPYNE_WORKDIR_PATH)
+
 with open("./experiment.json", "r") as f:
     exp = json.load(f)
-    exp["state"] = "SIMULATED"
 
 netParams = sim.loadNetParams("./netParams.json", None, False)
 simConfig = sim.loadSimCfg("./simConfig.json", None, False)
 
 sim.createSimulate(netParams, simConfig)
 sim.saveData()
-
+    
 with open("./experiment.json", "w") as f:
     if exp:
+        exp["state"] = "SIMULATED"
         json.dump(exp, f, default=str, sort_keys=True, indent=4)
