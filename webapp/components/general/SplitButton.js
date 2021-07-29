bd0e2d7b85eb15fc28c9dffc353caa11f3e06de5
Muhaddatha <mzabdulg@umich.edu>
2021-07-29 11:03:43 -0400

    #347 create split button component

---
+++ webapp/components/general/SplitButton.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+
+export default function SplitButton(props) {
+    const { options, handleClick, icon } = props;
+    const [open, setOpen] = React.useState(false);
+    const anchorRef = React.useRef(null);
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
+
+    const handleItemClick = (event) => {
+        setSelectedIndex(0);
+        handleClick(options[0]);
+    }
+
+    const handleMenuItemClick = (index) => {
+        setSelectedIndex(index);
+        setOpen(false);
+        handleClick(options[index]);
+    }
+
+    const handleToggle = () => {
+        setOpen((prevOpen) => !prevOpen);
+    }
+
+    const handleClose = (event) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target)) {
+            return;
+        }
+        setOpen(false);
+    }
+
+    return(
+        <Grid container direction="column" alignItems="center">
+            <Grid item xs={12}>
+                <ButtonGroup variant="contained" color="primary" ref={anchorRef} aria-label="split button">
+                    <Button onClick={handleItemClick}>{options[0]}{icon}</Button>
+                    <Button color="primary" size="small" aria-controls={open ? 'split-button-menu' : undefined}
+                        aria-expanded={open ? 'true' : undefined}
+                        aria-label="select merge strategy"
+                        aria-haspopup="menu"
+                        onClick={handleToggle}
+                        >
+                            <ArrowDropDownIcon />
+                        </Button>
+                </ButtonGroup>
+                <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+                    {({TransitionProps, placement}) => (
+                        <Grow {...TransitionProps} style={{transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom',}}>
+                            <Paper>
+                                <ClickAwayListener onClickAway={handleClose}>
+                                    <MenuList id="split-button-menu">
+                                        {options.map((option, index) => 
+                                            (<MenuItem key={option} selected={index === selectedIndex} onClick={(event) => {setSelectedIndex(index); handleMenuItemClick(index)}}>
+                                                {option}
+                                            </MenuItem>
+                                            ))}
+                                    </MenuList>
+                                </ClickAwayListener>
+                            </Paper>
+                        </Grow>
+                    )}
+                </Popper>
+            </Grid>
+        </Grid>
+    );
+}
