9c18b72a820234e4cf351d66040a9105217751fc
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-10 07:17:40 +0000

    Adding tarfile member sanitization to extractall()

--- netpyne_ui/api.py
+++ netpyne_ui/api.py
@@ -74,7 +74,26 @@ class NetPyNEController:  # pytest: no cover
 
                 elif filename.endswith('.tar.gz'):
                     with tarfile.open(file_path, mode='r:gz') as tar:
-                        tar.extractall(UPLOAD_FOLDER_PATH)
+                        def is_within_directory(directory, target):
+                            
+                            abs_directory = os.path.abspath(directory)
+                            abs_target = os.path.abspath(target)
+                        
+                            prefix = os.path.commonprefix([abs_directory, abs_target])
+                            
+                            return prefix == abs_directory
+                        
+                        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                        
+                            for member in tar.getmembers():
+                                member_path = os.path.join(path, member.name)
+                                if not is_within_directory(path, member_path):
+                                    raise Exception("Attempted Path Traversal in Tar File")
+                        
+                            tar.extractall(path, members, numeric_owner=numeric_owner) 
+                            
+                        
+                        safe_extract(tar, UPLOAD_FOLDER_PATH)
 
                 elif filename.endswith('.gz'):
                     with gzip.open(file_path, "rb") as gz, open(file_path.replace('.gz', ''), 'wb') as ff:
