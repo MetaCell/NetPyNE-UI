352051e8f19509d8b3f5ce5b18a0e4a3d6be74c0
enicolasgomez <enicolasgomez@gmail.com>
2022-11-22 10:22:03 -0300

    Fixed plots

--- webapp/components/general/PlotViewer.js
+++ webapp/components/general/PlotViewer.js
@@ -21,8 +21,8 @@ const PlotViewer = ({ key, id, method }) => {
             name="dipole"
             srcDoc={data}
             style={{
-              overflowX:hidden,
-              overflowY:hidden,
+              overflowX:'hidden',
+              overflowY:'hidden',
               border: 0,
               width: '100%',
               height: '100%',
