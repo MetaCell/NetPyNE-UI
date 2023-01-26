78c15643a6bc01b75f16052bcf5e6aab45f013a9
Simão sá <simao@metacell.us>
2023-01-26 11:11:23 +0000

    NETPYNE-95 step for plotting EEG and Dipole

--- tests/frontend/e2e/tests/EEG_and_Dipole_Tut#1.test.js
+++ tests/frontend/e2e/tests/EEG_and_Dipole_Tut#1.test.js
@@ -35,7 +35,7 @@ const CONNECTIONS_PLOT_SELECTOR = 'div[title=\"Connections Plot\"][role=button]'
 
 
 //USERS:
-const USERNAME = 'EEG_and_Dipole_Test_User_213'
+const USERNAME = 'EEG_and_Dipole_Userr_'
 const PASSWORD = 'password'
 
 
@@ -137,6 +137,54 @@ describe('EEG and Dipole Plot Test using Tutorial#1', () => {
         await page.waitForSelector('div[title="Raster plot"][aria-disabled="false"]', { timeout: TIMEOUT * 3 })
         await page.waitForSelector('div[title="EEG plot"][aria-disabled="false"]', { timeout: TIMEOUT * 3 })
         await page.waitForSelector('div[title="Dipole plot"][aria-disabled="false"]', { timeout: TIMEOUT * 3 })
+      });
+
+      it('Dipole Plot', async () => {
+
+        await page.waitForTimeout(PAGE_WAIT * 2);
+        await click(page, 'div[title="Dipole plot"][aria-disabled="false"]')
+        await page.waitForSelector('canvas', { timeout: TIMEOUT })
+
+        await console.log('View Dipole Plot ...')
+
+        await page.waitForTimeout(PAGE_WAIT * 10);
+        await click(page, CONNECTIONS_PLOT_SELECTOR, { timeout: TIMEOUT })
+        await page.waitForTimeout(PAGE_WAIT);
+        await click(page, 'div[title="Dipole plot"][aria-disabled="false"]')
+        await page.waitForSelector('canvas', { timeout: TIMEOUT })
+        await page.waitForTimeout(PAGE_WAIT * 3);
+    
+        await console.log('... taking snapshot ...');
+        expect(await page.screenshot())
+          .toMatchImageSnapshot({
+            ...SNAPSHOT_OPTIONS,
+            customSnapshotIdentifier: 'Dipole Plot'
+          });
+      });
+    
+    
+      it('EEG Plot', async () => {
+    
+        await page.waitForTimeout(PAGE_WAIT * 2);
+        await click(page, 'div[title="EEG plot"][aria-disabled="false"]')
+        await page.waitForSelector('canvas', { timeout: TIMEOUT })
+
+        await console.log('View EEG Plot ...')
+
+        await page.waitForTimeout(PAGE_WAIT * 12);
+        await click(page, CONNECTIONS_PLOT_SELECTOR, { timeout: TIMEOUT })
+        await page.waitForTimeout(PAGE_WAIT*2);
+        await click(page, 'div[title="EEG plot"][aria-disabled="false"]')
+        await page.waitForSelector('canvas', { timeout: TIMEOUT })
+        await page.waitForTimeout(PAGE_WAIT * 3);
+    
+        await console.log('... taking snapshot ...');
+        expect(await page.screenshot())
+          .toMatchImageSnapshot({
+            ...SNAPSHOT_OPTIONS,
+            customSnapshotIdentifier: 'EEG Plot'
+          });
+    
       });
 
    
