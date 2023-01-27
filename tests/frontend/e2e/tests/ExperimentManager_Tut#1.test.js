46e52e2576f02890cc828b2807cf34da97fd0569
Simão sá <simao@metacell.us>
2023-01-27 12:56:40 +0000

    netpyne-95 Setting up Experiment Manager test

---
+++ tests/frontend/e2e/tests/ExperimentManager_Tut#1.test.js
@@ -0,0 +1,115 @@
+//IMPORTS:
+import 'expect-puppeteer';
+import { click } from './utils';
+import { toMatchImageSnapshot } from 'jest-image-snapshot'
+expect.extend({ toMatchImageSnapshot })
+const path = require('path');
+var scriptName = path.basename(__filename, '.js');
+import * as selectors from './selectors'
+
+
+//PAGE INFO:
+const baseURL = process.env.url || 'https://stage.netpyne.metacell.us/'
+const PAGE_WAIT = 3000;
+const TIMEOUT = 60000;
+
+//SNAPSHOT:
+const SNAPSHOT_OPTIONS = {
+    customSnapshotsDir: `./tests/snapshots/${scriptName}`,
+    comparisonMethod: 'ssim',
+    customDiffConfig: {
+        ssim: 'fast',
+    },
+    failureThresholdType: 'percent',
+    failureThreshold: 0.05
+};
+
+
+
+//SELECTORS:
+const BASE_PAGE_SELECTOR = '.NetPyNE-root-1'
+const TUTORIALS_BUTTON_SELECTOR = 'button[id = "Tutorials"]'
+const TUTORIAL_3A_SELECTOR = 'li[id= "Tut 3a: Multiscale network (low IP3)"]'
+const MODEL_BUTTON_SELECTOR = 'button[id="Model"]'
+const CREATE_NETWORK_SELECTOR = 'li[id="Create network"]'
+const SIMULATE_NETWORK_SELECTOR = 'li[id="Simulate network"]'
+const SIMULATION_PAGE_SELECTOR = 'canvas'
+
+
+
+//USERS:
+const USERNAME = 'TestUser_ExperimentManager_'
+const PASSWORD = 'testpassword'
+
+
+//TESTS:
+
+jest.setTimeout(300000);
+
+
+
+describe('Experiment Manager test using Tut#1', () => {
+
+  beforeAll(async () => {
+    await page.goto(baseURL);
+    await page.waitForSelector('#login-main');
+    await page.waitForSelector('#username_input')
+    await expect(page)
+      .toFill('#username_input', USERNAME, { timeout: TIMEOUT });
+
+    await page.waitForSelector('#password_input')
+    await expect(page)
+      .toFill('#password_input', PASSWORD, { timeout: TIMEOUT });
+
+    await page.click('#login_submit')
+    // Wait for initial loading spinner to disappear
+    await page.waitForFunction(() => {
+      let el = document.querySelector('#loading-spinner');
+      return el == null || el.clientHeight === 0;
+    }, { timeout: TIMEOUT });
+  });
+
+  it('Load Tutorial#1', async () => {
+
+    await page.waitForTimeout(PAGE_WAIT * 2)
+    await page.waitForSelector('#selectCellButton', { timeout: TIMEOUT })
+    await page.waitForTimeout(PAGE_WAIT)
+    await click(page, TUTORIALS_BUTTON_SELECTOR, { timeout: TIMEOUT })
+
+    console.log('Tutorial #1')
+    await click(page, "li[id='Tut 1: Simple cell network']", { timeout: TIMEOUT })
+    await page.waitForTimeout(PAGE_WAIT)
+    await page.waitForSelector('#pyr')
+    await page.waitForTimeout(PAGE_WAIT)
+
+
+  })
+
+
+  it('Create network', async () => {
+
+    await page.waitForSelector(MODEL_BUTTON_SELECTOR)
+    await click(page, MODEL_BUTTON_SELECTOR, { timeout: TIMEOUT });
+    await page.waitForSelector(CREATE_NETWORK_SELECTOR)
+    await click(page, CREATE_NETWORK_SELECTOR, { timeout: TIMEOUT });
+
+    console.log('Create network')
+
+    await page.waitForSelector('div[title="Raster plot"][aria-disabled="true"]', { timeout: TIMEOUT * 3 })
+
+    await page.waitForTimeout(PAGE_WAIT)
+
+    console.log('... taking snapshot ...');
+    await page.waitForTimeout(PAGE_WAIT);
+    expect(await page.screenshot())
+      .toMatchImageSnapshot({
+        ...SNAPSHOT_OPTIONS,
+        customSnapshotIdentifier: 'Tutorial#1 Network'
+      });
+  })
+
+  
+
+
+
+});
