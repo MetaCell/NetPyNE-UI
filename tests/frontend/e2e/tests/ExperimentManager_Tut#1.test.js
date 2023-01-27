f59b21b168d4c1642d47e132d3162ebefa31dcd4
Simão sá <simao@metacell.us>
2023-01-27 13:00:43 +0000

    netpyne-95 step for setting an experiment

--- tests/frontend/e2e/tests/ExperimentManager_Tut#1.test.js
+++ tests/frontend/e2e/tests/ExperimentManager_Tut#1.test.js
@@ -38,7 +38,7 @@ const SIMULATION_PAGE_SELECTOR = 'canvas'
 
 
 //USERS:
-const USERNAME = 'TestUser_ExperimentManager_'
+const USERNAME = 'TestUser_ExperimentManager_1'
 const PASSWORD = 'testpassword'
 
 
@@ -50,65 +50,139 @@ jest.setTimeout(300000);
 
 describe('Experiment Manager test using Tut#1', () => {
 
-  beforeAll(async () => {
-    await page.goto(baseURL);
-    await page.waitForSelector('#login-main');
-    await page.waitForSelector('#username_input')
-    await expect(page)
-      .toFill('#username_input', USERNAME, { timeout: TIMEOUT });
+    beforeAll(async () => {
+        await page.goto(baseURL);
+        await page.waitForSelector('#login-main');
+        await page.waitForSelector('#username_input')
+        await expect(page)
+            .toFill('#username_input', USERNAME, { timeout: TIMEOUT });
 
-    await page.waitForSelector('#password_input')
-    await expect(page)
-      .toFill('#password_input', PASSWORD, { timeout: TIMEOUT });
+        await page.waitForSelector('#password_input')
+        await expect(page)
+            .toFill('#password_input', PASSWORD, { timeout: TIMEOUT });
 
-    await page.click('#login_submit')
-    // Wait for initial loading spinner to disappear
-    await page.waitForFunction(() => {
-      let el = document.querySelector('#loading-spinner');
-      return el == null || el.clientHeight === 0;
-    }, { timeout: TIMEOUT });
-  });
+        await page.click('#login_submit')
+        // Wait for initial loading spinner to disappear
+        await page.waitForFunction(() => {
+            let el = document.querySelector('#loading-spinner');
+            return el == null || el.clientHeight === 0;
+        }, { timeout: TIMEOUT });
+    });
 
-  it('Load Tutorial#1', async () => {
+    it('Load Tutorial#1', async () => {
 
-    await page.waitForTimeout(PAGE_WAIT * 2)
-    await page.waitForSelector('#selectCellButton', { timeout: TIMEOUT })
-    await page.waitForTimeout(PAGE_WAIT)
-    await click(page, TUTORIALS_BUTTON_SELECTOR, { timeout: TIMEOUT })
+        await page.waitForTimeout(PAGE_WAIT * 2)
+        await page.waitForSelector('#selectCellButton', { timeout: TIMEOUT })
+        await page.waitForTimeout(PAGE_WAIT)
+        await click(page, TUTORIALS_BUTTON_SELECTOR, { timeout: TIMEOUT })
 
-    console.log('Tutorial #1')
-    await click(page, "li[id='Tut 1: Simple cell network']", { timeout: TIMEOUT })
-    await page.waitForTimeout(PAGE_WAIT)
-    await page.waitForSelector('#pyr')
-    await page.waitForTimeout(PAGE_WAIT)
-
-
-  })
+        console.log('Tutorial #1')
+        await click(page, "li[id='Tut 1: Simple cell network']", { timeout: TIMEOUT })
+        await page.waitForTimeout(PAGE_WAIT)
+        await page.waitForSelector('#pyr')
+        await page.waitForTimeout(PAGE_WAIT)
 
 
-  it('Create network', async () => {
+    })
 
-    await page.waitForSelector(MODEL_BUTTON_SELECTOR)
-    await click(page, MODEL_BUTTON_SELECTOR, { timeout: TIMEOUT });
-    await page.waitForSelector(CREATE_NETWORK_SELECTOR)
-    await click(page, CREATE_NETWORK_SELECTOR, { timeout: TIMEOUT });
 
-    console.log('Create network')
+    it('Create network', async () => {
 
-    await page.waitForSelector('div[title="Raster plot"][aria-disabled="true"]', { timeout: TIMEOUT * 3 })
+        await page.waitForSelector(MODEL_BUTTON_SELECTOR)
+        await click(page, MODEL_BUTTON_SELECTOR, { timeout: TIMEOUT });
+        await page.waitForSelector(CREATE_NETWORK_SELECTOR)
+        await click(page, CREATE_NETWORK_SELECTOR, { timeout: TIMEOUT });
 
-    await page.waitForTimeout(PAGE_WAIT)
+        console.log('Create network')
 
-    console.log('... taking snapshot ...');
-    await page.waitForTimeout(PAGE_WAIT);
-    expect(await page.screenshot())
-      .toMatchImageSnapshot({
-        ...SNAPSHOT_OPTIONS,
-        customSnapshotIdentifier: 'Tutorial#1 Network'
-      });
-  })
+        await page.waitForSelector('div[title="Raster plot"][aria-disabled="true"]', { timeout: TIMEOUT * 3 })
 
-  
+        await page.waitForTimeout(PAGE_WAIT)
+
+        console.log('... taking snapshot ...');
+        await page.waitForTimeout(PAGE_WAIT);
+        expect(await page.screenshot())
+            .toMatchImageSnapshot({
+                ...SNAPSHOT_OPTIONS,
+                customSnapshotIdentifier: 'Tutorial#1 Network'
+            });
+    })
+
+    it('Set an Experiment in the Experiment Manager ', async () => {
+
+        console.log('Setting up experiment')
+
+        await page.waitForSelector('div[title="Experiment Manager"]')
+        await page.click('div[title="Experiment Manager"]')
+        await page.waitForSelector('button[class="MuiButtonBase-root MuiButton-root MuiButton-text MuiButton-textPrimary"]')
+        await page.click('button[class="MuiButtonBase-root MuiButton-root MuiButton-text MuiButton-textPrimary"]')
+
+        await page.waitForSelector('h2[class="MuiTypography-root MuiTypography-h6"]')
+        await page.click('#confirmDeletion')
+        await page.waitForSelector('#experiment-name')
+
+        await expect(page).toFill('#experiment-name', 'Test Experiment')
+        await page.waitForTimeout(PAGE_WAIT);
+
+        await page.click('#undefined-combo-box-demo')
+
+
+        await page.evaluate(() => {
+            let parameter = document.querySelectorAll('li[class="MuiAutocomplete-option"]');
+            for (var i = 0; i < parameter.length; i++) {
+                parameter[i].innerHTML.includes("numCells") && parameter[i].click();
+            }
+        });
+
+        await page.waitForTimeout(PAGE_WAIT);
+
+        const inputFromValue = await page.$eval('#undefined-from', el => el.value);
+        await page.click('#undefined-to');
+        for (let i = 0; i < inputFromValue.length; i++) {
+            await page.keyboard.press('Backspace');
+        }
+
+        await page.type('#undefined-from', '1')
+
+        const inputToValue = await page.$eval('#undefined-to', el => el.value);
+        await page.click('#undefined-to');
+        for (let i = 0; i < inputToValue.length; i++) {
+            await page.keyboard.press('Backspace');
+        }
+
+        await page.type('#undefined-to', '4')
+
+        const inputStepValue = await page.$eval('#undefined-step', el => el.value);
+        await page.click('#undefined-step');
+        for (let i = 0; i < inputStepValue.length; i++) {
+            await page.keyboard.press('Backspace');
+        }
+
+        await page.type('#undefined-step', '1')
+
+        await page.waitForTimeout(PAGE_WAIT);
+
+        await page.click('button[class="MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary"]')
+
+        await page.waitForSelector('th[class="MuiTableCell-root MuiTableCell-body"]')
+        await page.waitForTimeout(PAGE_WAIT);
+
+        const experiment_name = await page.evaluate(() => {
+            document.querySelector('h6[class="MuiTypography-root experimentHead MuiTypography-h6"]').textContent
+        });
+
+        const experimentName = await page.$eval('th[class="MuiTableCell-root MuiTableCell-body"]', el => el.innerText.trim());
+        await page.waitForTimeout(PAGE_WAIT);
+        expect(experimentName).toBe('Test_Experiment')
+        await page.waitForTimeout(PAGE_WAIT);
+
+        console.log('Experiment created')
+
+    })
+
+
+
+
 
 
 
