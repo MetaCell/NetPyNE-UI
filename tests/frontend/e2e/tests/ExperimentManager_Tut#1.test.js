5053c1c77e91a7a8e264afc95c1a889cdfbb4a9e
Simão sá <simao@metacell.us>
2023-01-27 13:11:18 +0000

    netpyne-95 steps for checking the experiment results

--- tests/frontend/e2e/tests/ExperimentManager_Tut#1.test.js
+++ tests/frontend/e2e/tests/ExperimentManager_Tut#1.test.js
@@ -38,7 +38,7 @@ const SIMULATION_PAGE_SELECTOR = 'canvas'
 
 
 //USERS:
-const USERNAME = 'TestUser_ExperimentManager_12'
+const USERNAME = 'TestUser_ExperimentManager_124'
 const PASSWORD = 'testpassword'
 
 
@@ -184,25 +184,138 @@ describe('Experiment Manager test using Tut#1', () => {
 
         await page.waitForSelector('div[class="MuiButtonGroup-root MuiButtonGroup-contained"]')
         await click(page, 'div[class="MuiButtonGroup-root MuiButtonGroup-contained"]', { timeout: TIMEOUT });
-    
+
         console.log('Simulating all conditions')
-    
+
         await page.waitForSelector('div[class="MuiBox-root MuiBox-root-185 wrap"]')
         await page.click('div[class="MuiBox-root MuiBox-root-185 wrap"]')
         await page.click('#appBarPerformActionButton')
-    
+
         await page.waitForTimeout(PAGE_WAIT);
-    
+
         await page.waitForSelector('button[class="MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary"]')
         await page.click('button[class="MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary"]')
-    
+
         await page.waitForSelector(SIMULATION_PAGE_SELECTOR, { timeout: TIMEOUT * 2 });
         await page.waitForSelector('div[class = "MuiBox-root MuiBox-root-186 MuiChip-icon MuiChipLoader"]', { hidden: false, timeout: TIMEOUT * 2 })
         await page.waitForSelector('div[class = "MuiBox-root MuiBox-root-186 MuiChip-icon MuiChipLoader"]', { hidden: true, timeout: TIMEOUT * 5 })
         console.log('Experiment Simulation finished')
-    
-      });
-    
+
+    });
+
+    it('Check Experiment Condition #1', async () => {
+
+        console.log('Checking experiment condition #1')
+
+        await page.click('span[class="MuiButton-startIcon MuiButton-iconSizeMedium"]')
+
+        await page.waitForTimeout(PAGE_WAIT * 2)
+
+        await page.waitForSelector('div[title="Cell Types"]')
+        await page.click('div[title="Cell Types"]')
+
+        await page.evaluate(() => {
+            let sections = document.querySelectorAll('div[class="MuiButtonBase-root MuiListItem-root makeStyles-selected-23 MuiListItem-dense MuiListItem-button"]');
+            for (var i = 0; i < sections.length; i++) {
+                sections[i].textContent.includes("Experiment Manager") && sections[i].click();
+            }
+        });
+
+        await page.waitForTimeout(PAGE_WAIT)
+
+
+        await page.waitForSelector('table[class="MuiTable-root"]')
+        await page.click('button[class="MuiButtonBase-root MuiButton-root MuiButton-text"]')
+
+        await page.waitForSelector('tr[class = "MuiTableRow-root MuiTableRow-head"]')
+
+        await page.evaluate(() => {
+            let results = document.querySelectorAll('button[title="Explore results"]');
+            for (var i = 0; i < results.length; i++) {
+                results[0].innerHTML.includes("label") && results[0].click();
+            }
+        });
+
+        await page.waitForSelector('#confirmDeletion')
+        await page.click('#confirmDeletion')
+        await page.waitForTimeout(PAGE_WAIT)
+
+        console.log('... taking snapshot ...');
+        await page.waitForTimeout(PAGE_WAIT);
+        expect(await page.screenshot())
+            .toMatchImageSnapshot({
+                ...SNAPSHOT_OPTIONS,
+                customSnapshotIdentifier: 'Experiment Condition #1'
+            });
+
+
+    })
+
+
+
+    it('Check Experiment Condition #2', async () => {
+
+        console.log('Checking experiment condition #2')
+
+
+        await page.waitForTimeout(PAGE_WAIT)
+        await page.click('span[class="MuiButton-startIcon MuiButton-iconSizeMedium"]')
+
+        await page.waitForTimeout(PAGE_WAIT * 3)
+
+        await page.evaluate(() => {
+            let results = document.querySelectorAll('button[title="Explore results"]');
+            for (var i = 0; i < results.length; i++) {
+                results[1].innerHTML.includes("label") && results[1].click();
+            }
+        });
+
+        await page.waitForSelector('#confirmDeletion')
+        await page.click('#confirmDeletion')
+        await page.waitForTimeout(PAGE_WAIT)
+
+        console.log('... taking snapshot ...');
+        await page.waitForTimeout(PAGE_WAIT);
+        expect(await page.screenshot())
+            .toMatchImageSnapshot({
+                ...SNAPSHOT_OPTIONS,
+                customSnapshotIdentifier: 'Experiment Condition #2'
+            });
+
+
+    })
+
+    it('Check Experiment Condition #3', async () => {
+
+        console.log('Checking experiment condition #3')
+
+        await page.waitForTimeout(PAGE_WAIT)
+        await page.click('span[class="MuiButton-startIcon MuiButton-iconSizeMedium"]')
+
+        await page.waitForTimeout(PAGE_WAIT * 3)
+
+        await page.evaluate(() => {
+            let results = document.querySelectorAll('button[title="Explore results"]');
+            for (var i = 0; i < results.length; i++) {
+                results[2].innerHTML.includes("label") && results[2].click();
+            }
+        });
+
+        await page.waitForSelector('#confirmDeletion')
+        await page.click('#confirmDeletion')
+        await page.waitForTimeout(PAGE_WAIT)
+
+        console.log('... taking snapshot ...');
+        await page.waitForTimeout(PAGE_WAIT);
+        expect(await page.screenshot())
+            .toMatchImageSnapshot({
+                ...SNAPSHOT_OPTIONS,
+                customSnapshotIdentifier: 'Experiment Condition #3'
+            });
+
+
+    })
+
 
 
 
