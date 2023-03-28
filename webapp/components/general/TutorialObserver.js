836ed396c0a1e48c9527ef19e24df873f384f67e
enicolasgomez <enicolasgomez@gmail.com>
2023-03-28 09:21:11 -0300

    Removed dome parsing and lists

--- webapp/components/general/TutorialObserver.js
+++ webapp/components/general/TutorialObserver.js
@@ -6,12 +6,9 @@ export default function TutorialObserver(props) {
 
   const {
     steps,
-    tourStep,
-    tourRunning,
     requestedTourStep,
     startTutorialStep,
     stopTutorialStep,
-    addDiscoveredStep,
     incrementTutorialStep,
     validateTutorialStep,
     checkBubbleRender,
@@ -19,48 +16,15 @@ export default function TutorialObserver(props) {
     children
   } = props;
 
-  const [ nodeIdList, setNodeIdList] = useState([]);
-
   const bubbleUpdate = () => {
     checkBubbleRender({ epoch: Date.now() })
-  };
-
-  const search =  tutorial_steps.map( s => s.target ); 
-
-  const traverseNodes = (nodes, search) => {
-    nodes.forEach((node) => {
-      // Check if the node has the desired property
-      search.forEach( s => {
-        const attr_name  = s.indexOf('#') > -1 ? 'id' : 'className';
-        const attr_value = s.split(/[#]/).join("");
-        if (node[attr_name] === attr_value) {
-          // Add a new step to the steps array
-          let nodeId = node[attr_name] ;
-          if (nodeIdList.indexOf(nodeId) == -1)
-            setNodeIdList([...nodeIdList, nodeId]);
-        }
-      })
-
-      // Recursively traverse the node's children
-      if (node.childNodes) {
-        traverseNodes(node.childNodes, search);
-      }
-    });
   };
 
   useEffect(() => {
     // Listen for new components being added to the DOM
     const observer = new MutationObserver((mutationsList) => {
-      mutationsList.forEach((mutation) => {
-        if (
-          mutation.type === "childList" &&
-          mutation.addedNodes.length > 0
-        ) {
-          // Traverse the added nodes and their descendants
-          traverseNodes(mutation.addedNodes, search);
-        }
-      });
-      bubbleUpdate();
+      if(mutationsList.length > 0)
+        bubbleUpdate();
     });
 
     // Start observing the DOM
@@ -69,9 +33,6 @@ export default function TutorialObserver(props) {
     // Stop observing the DOM when the component unmounts
     return () => observer.disconnect();
   }, []);
-
-  if(nodeIdList.length > 0)
-    addDiscoveredStep({ nodeIdList })
 
   const startTutorialCallBack = () => {
     startTutorialStep();  
--- webapp/redux/reducers/tutorial.js
+++ webapp/redux/reducers/tutorial.js
@@ -1,7 +1,7 @@
 // import action types
 import React from 'react';
 import tutorial_steps from './data/tutorial_steps';
-import { START_TUTORIAL, STOP_TUTORIAL, INCREMENT_TUTORIAL_STEP, RUN_CONTROLLED_STEP, ADD_DISCOVERED_STEP, RUN_CONTROLLED_STEP_BY_ELEMENT_ID, VALIDATE_TUTORIAL_STEP, CHECK_BUBBLE_RENDER } from '../actions/tutorials';
+import { START_TUTORIAL, STOP_TUTORIAL, INCREMENT_TUTORIAL_STEP, RUN_CONTROLLED_STEP, VALIDATE_TUTORIAL_STEP, CHECK_BUBBLE_RENDER } from '../actions/tutorials';
 
 // Default state for general
 export const TUTORIAL_DEFAULT_STATE = {
@@ -9,24 +9,9 @@ export const TUTORIAL_DEFAULT_STATE = {
   requestedTourStep: 0,
   lastCheckRender: 0,
   tourStep: 0,
-  activeSteps: [],
-  discoveredSteps: [],
   steps: tutorial_steps,
   renderedSteps: []
 };
-
-//component could fire a increment step but it might not yet be active on the observable
-function getValidActiveStep( requiredStep, currentStep, steps, discoveredSteps )
-{
-  let tourStep = currentStep ;
-  if (requiredStep > 0)
-  {
-    const requestedStepConfig = steps[requiredStep-1];
-    if ( discoveredSteps.indexOf(requestedStepConfig.target.replace('#', '')) > -1)
-      tourStep = requiredStep ;
-  }
-  return tourStep ;
-}
 
 // reducer
 export default (state = TUTORIAL_DEFAULT_STATE, action) => {
@@ -35,20 +20,6 @@ export default (state = TUTORIAL_DEFAULT_STATE, action) => {
       return Object.assign(state, { tourRunning: true, requestedTourStep: 1 });
     case STOP_TUTORIAL:
       return Object.assign(state, { tourRunning: false });
-    case ADD_DISCOVERED_STEP:
-    {
-      const discoveredSteps = [...state.discoveredSteps, ...action.payload.nodeIdList].filter((item, index, arr) => arr.indexOf(item) === index);
-      return { 
-        ...state,
-        discoveredSteps,
-        activeSteps: [...action.payload.nodeIdList]
-      }
-    }
-    case RUN_CONTROLLED_STEP_BY_ELEMENT_ID:
-    {
-      const stepIndex = state.steps.findIndex( step => step.target == action.payload)
-      return stepIndex > -1 ? Object.assign(state, { tourRunning: true, tourStep: stepIndex+1 }) : state ;
-    }
     case INCREMENT_TUTORIAL_STEP:
     {
       const requestedTourStep = state.tourStep + 1 ;
