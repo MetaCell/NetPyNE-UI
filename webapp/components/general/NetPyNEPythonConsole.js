3efc8be0e239e05d27666624273e86abac818aae
ddelpiano <dario@metacell.us>
2023-05-03 11:27:56 +0100

    NETPYNE-150: python console height fixed

--- webapp/components/general/NetPyNEPythonConsole.js
+++ webapp/components/general/NetPyNEPythonConsole.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-
+import ReactResizeDetector from 'react-resize-detector';
 import { PythonConsole } from '@metacell/geppetto-meta-ui/python-console/PythonConsole';
 
 export class NetPyNEPythonConsole extends Component {
-
   shouldComponentUpdate (nextProps) {
     if (this.props.extensionLoaded !== nextProps.extensionLoaded) {
       return true;
@@ -11,17 +10,17 @@ export class NetPyNEPythonConsole extends Component {
     return false;
   }
 
-  componentWillUnmount() {
-    console.info("unmounting python console");
+  componentWillUnmount () {
+    console.info('unmounting python console');
   }
 
-  componentDidMount() {
-
-  }
-
-  render() {
+  render () {
     const notebookName = GEPPETTO_CONFIGURATION.notebookName || "notebook.ipynb";
-    return <PythonConsole pythonNotebookPath={`notebooks/${notebookName}`} extensionLoaded={this.props.extensionLoaded} />
+    return (
+      <ReactResizeDetector handleWidth handleHeight>
+        {({ width, height }) => <PythonConsole pythonNotebookPath={`notebooks/${notebookName}`} extensionLoaded={this.props.extensionLoaded} iframeHeight={height} />}
+      </ReactResizeDetector>
+    );
   }
 }
 
